a50ec48a519bc988667695e1a3770d5b2a8845f8
Tarik El-Yassem <tarik@openvas.org>
2006-05-23 10:03:30 +0000

    Replaced the header.
    git-svn-id: svn+ssh://scm.wald.intevation.org/openvas/trunk/openvas-server@125 423fd1db-d629-0410-8442-d21db03e70f4

--- openvasd/plugs_hash.c
+++ openvasd/plugs_hash.c
@@ -1,25 +1,28 @@
 /* OpenVAS
- * Copyright (C) 1998 - 2004 Renaud Deraison
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
- *
- *
- * This module computes the hash of the plugins (and the hash of the
- * hashes of the plugins)
- *
- */
+* $Id$
+* Description: Calculates hash values of plugins, calculates the hash value of all hashes.
+*
+* Authors: - Laban Mwangi <labaneator@gmail.com> (initial version)
+*
+* Copyright:
+* Portions Copyright (C) 2006 Software in the Public Interest, Inc.
+* Based on work Copyright (C) 1998 - 2006 Tenable Network Security, Inc.
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License version 2,
+* as published by the Free Software Foundation
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+*
+*
+*/
 
 #include <includes.h>
 #include "./md5.h"
