16ee6df0b252cfa59089c257286c817a06d38226
Tarik El-Yassem <tarik@openvas.org>
2006-05-23 10:42:56 +0000

    Replaced the headers.
    git-svn-id: svn+ssh://scm.wald.intevation.org/openvas/trunk/openvas-server@127 423fd1db-d629-0410-8442-d21db03e70f4

--- openvasd/openvas-check-signature.c
+++ openvasd/openvas-check-signature.c
@@ -1,34 +1,36 @@
 /* OpenVAS
- * Copyright (C) 1998 - 2004 Renaud Deraison
- * Copyright (C) 2005 Tim Brown
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2,
- * as published by the Free Software Foundation
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
- *
- * 
- * This very simply utility generates/checks a signature for a given file
- *
- *
- * $Id$
- */
+* $Id$
+* Description: generates/checks a signature for a given file.
+*
+* Authors: - 	Tim Brown <timb@openvas.org> (intitial version)
+		Laban Mwangi <labeneator@gmail.com> (initial version)
+*
+* Copyright:
+* Portions Copyright (C) 2006 Software in the Public Interest, Inc.
+* Based on work Copyright (C) 1998 - 2006 Tenable Network Security, Inc.
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License version 2,
+* as published by the Free Software Foundation
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+*
+*
+*/
+
 #include <includes.h>
 #ifdef HAVE_SSL
 #include <openssl/bn.h>
 #include <openssl/dh.h>
 #include <openssl/evp.h>
-
 #include <openssl/blowfish.h>
-
 
 
 /* 
--- openvasd/piic.c
+++ openvasd/piic.c
@@ -1,34 +1,34 @@
 /* OpenVAS
- * Copyright (C) 1998 - 2004 Renaud Deraison
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2,
- * as published by the Free Software Foundation
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
- *
- *
- * Plugins Inter Communication
- * -   -   -     -
- *
- * This set of functions just read what the plugin writes on its pipe,
- * and put it in an arglist
- */ 
+* $Id$
+* Description: Reads from plugin output pipe, writes to arglist.
+*
+* Authors: - Laban Mwangi <labeneator@gmail.com> (initial version)
+*
+* Copyright:
+* Portions Copyright (C) 2006 Software in the Public Interest, Inc.
+* Based on work Copyright (C) 1998 - 2006 Tenable Network Security, Inc.
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License version 2,
+* as published by the Free Software Foundation
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+*
+*
+*/
  
 #include <includes.h>
 #include "log.h"
 #include "save_kb.h"
 #include "utils.h"
 #include "piic.h"
-
-
 
 
 void kb_parse(int soc, struct arglist * globals, struct kb_item ** kb, char * buf, int msg )
@@ -40,9 +40,7 @@ void kb_parse(int soc, struct arglist * globals, struct kb_item ** kb, char * bu
  char * copy;
  char * name;
  char * value;
-
-
-
+ 
  if( buf == NULL || kb == NULL )
   return;
 
