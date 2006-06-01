eeda24d49ad04a797773946d874e035386f9f1db
Tarik El-Yassem <tarik@openvas.org>
2006-06-01 11:34:47 +0000

    Added stubheader even though I'm not sure these files are still needed.
    git-svn-id: svn+ssh://scm.wald.intevation.org/openvas/trunk/openvas-server@141 423fd1db-d629-0410-8442-d21db03e70f4

--- include/nessusip.h
+++ include/nessusip.h
@@ -1,5 +1,34 @@
-#ifndef NESSUS_IP_H__
+/* OpenVAS
+* $Id$
+* Description: Defines IP structs.
+*
+* Authors: - Renaud Deraison <deraison@nessus.org> (Original pre-fork develoment)
+*          - Tim Brown <mailto:timb@openvas.org> (Initial fork)
+*          - Laban Mwangi <mailto:labanm@openvas.org> (Renaming work)
+*          - Tarik El-Yassem <mailto:tarik@openvas.org> (Headers section)
+*
+* Copyright:
+* Portions Copyright (C) 2006 Software in the Public Interest, Inc.
+* Based on work Copyright (C) 1998 - 2006 Tenable Network Security, Inc.
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License version 2,
+* as published by the Free Software Foundation
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+*
+*
+*/
 
+
+#ifndef NESSUS_IP_H__
 #ifdef HAVE_NETINET_IP_H
 #include <netinet/ip.h>
 #endif
--- include/nessustcp.h
+++ include/nessustcp.h
@@ -1,6 +1,35 @@
+/* OpenVAS
+* $Id$
+* Description: defines for TCP/IP flags and network byte order.
+*
+* Authors: - Renaud Deraison <deraison@nessus.org> (Original pre-fork develoment)
+*          - Tim Brown <mailto:timb@openvas.org> (Initial fork)
+*          - Laban Mwangi <mailto:labanm@openvas.org> (Renaming work)
+*          - Tarik El-Yassem <mailto:tarik@openvas.org> (Headers section)
+*
+* Copyright:
+* Portions Copyright (C) 2006 Software in the Public Interest, Inc.
+* Based on work Copyright (C) 1998 - 2006 Tenable Network Security, Inc.
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License version 2,
+* as published by the Free Software Foundation
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+*
+*
+*/
+
+
 #ifndef NESSUS_TCP_H__
 #define NESSUS_TCP_H__
-
 
 #ifdef HAVE_NETINET_TCP_H
 #include <netinet/tcp.h>
@@ -9,7 +38,6 @@
 #ifdef _CYGWIN_
 #define tcp_seq u_int
 #endif
-
 
 #if !defined(HAVE_STRUCT_TCPHDR) || (HAVE_STRUCT_TCPHDR == 0)
 #undef HAVE_TCPHDR_TH_X2_OFF
