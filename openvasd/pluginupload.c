04a51c5f5f3d658562ff958d785fd3de11f69476
Bernhard Herzog <bernhard.herzog@intevation.de>
2008-05-08 15:48:49 +0000

    * openvasd/pluginupload.c (plugin_recv): Always discard the uploaded data and reject the plugin upload.  This change removes support for plugin upload without changing the protocol (plugins_homedir): Removed. No longer used.
    git-svn-id: svn+ssh://scm.wald.intevation.org/openvas/trunk/openvas-server@860 423fd1db-d629-0410-8442-d21db03e70f4

--- openvasd/pluginupload.c
+++ openvasd/pluginupload.c
@@ -33,157 +33,65 @@
 #include "users.h"
 #include "log.h"
 #include "pluginload.h"
-/*
- * Where the plugins will be stored on the remote server
- * (<userhome>/plugins)
- */ 
-static char *
-plugins_homedir(globals)
- struct arglist * globals;
-{ 
- char * uhome = user_home(globals);
- char * ret = emalloc(strlen(uhome) + strlen("plugins") + 2);
- sprintf(ret, "%s/plugins", uhome);
- efree(&uhome);
- return ret;
-}
 
 
 int
-plugin_recv(globals)
- struct arglist * globals;
+plugin_recv(struct arglist * globals)
 {
- int soc = (int)arg_get_value(globals, "global_socket");
- struct arglist * preferences = arg_get_value(globals, "preferences");
- char * name;
- int n;
- long bytes = 0;
- int fd;
- char fullname[1024], tmpname[1024];
- char input[4096];
- char * buffer;
- 
-  
- n = recv_line(soc, input, sizeof(input) - 1);
- if(n <= 0)
-  return -1;
-  
- if(!strncmp(input, "name: ", strlen("name: ")))
-  {
-  name = estrdup(input + strlen("name: "));
-  if(name[strlen(name) - 1] == '\n')
-   name[strlen(name) - 1] = '\0';
-  }
-  else 
-   return -1;
-  
- /* 
-  * invalid file name
-  */
- if(strchr(name, '/'))
-  { 
-   log_write("%s - invalid file name\n", name);
-   return -1; 
-  }
+  int soc = (int)arg_get_value(globals, "global_socket");
+  int n;
+  long bytes = 0;
+  char input[4096];
 
- n = recv_line(soc, input, sizeof(input) - 1);
- if(n <= 0)
-  return -1;
- /* XXX content: message. Ignored for the moment */
- 
- n = recv_line(soc, input, sizeof(input) - 1);
- if(n <= 0)
-  return -1;
-  
- if(!strncmp(input, "bytes: ", strlen("bytes: ")))
- {
-  char * t = input + strlen("bytes: ");
-  bytes = atol(t);
- }
-  else return -1;
-  
-  
- /*
-  * Don't accept plugins bigger than 5Mb 
-  */
- if(bytes > 5*1024*1024)
-  return -1; 
-  
-  
- /*
-  * Ok. We now know that we have to read <bytes> bytes from the
-  * remote socket.
-  */
-  
-  if(preferences_upload_enabled(preferences) &&
-     preferences_upload_suffixes(preferences,
-     				name))
-  {
-  char * dir;
-  if(!preferences_user_is_admin(globals, preferences))
-   dir  = plugins_homedir(globals);
+  /* Read and ignore the "name:" line */
+  n = recv_line(soc, input, sizeof(input) - 1);
+  if (n <= 0)
+    return -1;
+  if (strncmp(input, "name: ", strlen("name: ")) != 0)
+    return -1;
+
+  /* Read and ignore the "content:" line */
+  n = recv_line(soc, input, sizeof(input) - 1);
+  if (n <= 0)
+    return -1;
+
+  /* Read and parse the "bytes:" line */
+  n = recv_line(soc, input, sizeof(input) - 1);
+  if (n <= 0)
+    return -1;
+
+  if (!strncmp(input, "bytes: ", strlen("bytes: ")))
+    {
+      char * t = input + strlen("bytes: ");
+      bytes = atol(t);
+    }
   else
-   dir = estrdup(arg_get_value(preferences, "plugins_folder"));
-  snprintf(fullname, sizeof(fullname), "%s/%s", dir, name);
-  snprintf(tmpname, sizeof(fullname), "%s/.%s.tmp", dir, name);
-  efree(&dir);
-  }
-  else {
-  	 strncpy(fullname, "/dev/null", sizeof(fullname) - 1);
-  	 strncpy(tmpname, "/dev/null", sizeof(tmpname) - 1);
+    return -1;
+
+  /* Don't accept plugins bigger than 5Mb */
+  if (bytes > 5*1024*1024)
+    return -1;
+
+  /*
+   * Read and discard the <bytes> bytes with the actual plugin.
+   */
+  while (bytes > 0)
+    {
+      int e;
+      int chunk_size = bytes < sizeof(input) ? bytes : sizeof(input);
+      e = read_stream_connection_min(soc, input, chunk_size, chunk_size);
+      if (e <= 0)
+	{
+	  if (errno == EINTR)
+	    continue;
+	  else
+	    break;
 	}
-  
-  log_write("saving in %s", fullname);
- 
-  fd = open(tmpname, O_CREAT|O_WRONLY|O_TRUNC, 0600);
-  if(fd < 0)
-  {
-   perror("plugins_recv(): open() ");
-   return -1;
-  }
-  
-  buffer = emalloc(bytes);
-  n = 0;
-  while (n != bytes)
-  { 
-   int e;
-   e = read_stream_connection_min(soc, buffer+n, bytes-n, bytes-n);
-   if (e <= 0)
-	    {
-		if(errno == EINTR)continue;
-     		else break;
-	    }		
-   n += e;
-  }
+      bytes -= e;
+    }
 
-  write(fd, buffer, n);
-  close(fd);
-  
-  
-  if(strcmp(fullname, "/dev/null"))
-   auth_printf(globals, "SERVER <|> PLUGIN_ACCEPTED <|> SERVER\n");
-  else
-   auth_printf(globals, "SERVER <|> PLUGIN_DENIED <|> SERVER\n");
-  
-  if( strcmp(fullname, tmpname) != 0 )
-  {
-   unlink(fullname);
-   if ( rename(tmpname, fullname) <  0 ) 
-   	perror("rename ");
-  }
-  
-  
-  efree(&buffer);
-#if 0
-  arg_set_value(globals, 
-  		"plugins", 
-		-1, 
-  		plugins_reload_user(
-			    globals, 
-			    preferences, 
-			    arg_get_value(globals, "plugins")
-			    )
-		);
-#endif
+  /* Always deny the upload since openvasd does not support it */
+  auth_printf(globals, "SERVER <|> PLUGIN_DENIED <|> SERVER\n");
+
   return 0;
 }
