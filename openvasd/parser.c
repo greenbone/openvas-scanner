2e633ed0b7a6a6f40f9253a8e28e1889d7925ba0
Felix Wolfsteller <felix.wolfsteller@greenbone.net>
2009-08-14 07:45:24 +0000

    * openvasd/parser.c: Added TODO because of code duplication in client. Added TODO about possible code improvement. Reformatted, added documentation. Removed number of calls to strlen.
    git-svn-id: svn+ssh://scm.wald.intevation.org/openvas/trunk/openvas-server@4493 423fd1db-d629-0410-8442-d21db03e70f4

--- openvasd/parser.c
+++ openvasd/parser.c
@@ -29,55 +29,90 @@
 
 #include <includes.h>
 
-/* 
- * This function extracts the string after the 
- * ' <|> ' symbol.
- *
+/** @TODO Contents of this file is duplicate in openvas-server/openvasd/parser.c
+ *        and openvas-client/nessus/parser.c . Move to libraries and merge, once
+ *        openvas-client depends on libraries. */
+
+/**
+ * @brief This function returns a pointer to the string after the ' \<|\> '
+ * @brief symbol.
+ * 
+ * @param str String that contains \<|\>.
+ * 
+ * @return A pointer into \ref str that points behind the \<|\>, or NULL
+ *         if none found.
  */
-char * 
-parse_symbol(str)
-	char * str;
+char *
+parse_symbol (char* str)
+
 {
- char * s = str;
- 
- while(s)
- {
- s = strchr(s, '|');
- if(!s)return(NULL);
- if((s[1]=='>')&&(s-1)[0]=='<')return(s+3);
- s++;
- }
- return(NULL);
+  char * s = str;
+
+  while (s)
+    {
+      s = strchr (s, '|');
+      if (!s)
+        return ( NULL );
+      if ((s[1] == '>') && (s-1)[0] == '<')
+        return (s+3);
+      s++;
+    }
+
+  return (NULL);
 }
 
-char * parse_separator(str)
-	char * str;
+/**
+ * @brief In the standard case, returns content between two separators (\<|\>)
+ * @brief in a string.
+ * 
+ * Returns a copy of the string between the first two '\<|\>'s.
+ * If just one \<|\> is found, returns a copy of the string from the first
+ * \<|\> till its end.
+ * Returns NULL if str is empty or does not contain a ' <|> '.
+ * 
+ * @param str String to parse.
+ * 
+ * @return Copy of content of string between two separators, see detailed doc
+ *         for special cases.
+ */
+char *
+parse_separator (char * str)
 {
- char * s_1, *s_2;
- char * ret;
- 
- s_1 = parse_symbol(str);
- if(!s_1)return(NULL);
- s_2 = parse_symbol(s_1);
- if(!s_2)
-  {
-  ret = emalloc(strlen(s_1));
-  strncpy(ret, s_1, strlen(s_1)-1);
-  }
- else
- {
- int c;
- s_2 = s_2 - 4;
- c = s_2[0];
- s_2[0] = 0;
- ret = emalloc(strlen(s_1));
- strncpy(ret, s_1, strlen(s_1)-1);
- s_2[0]=c;
- }
+  char * s_1;
+  char * s_2;
+  char * ret;
+  int len = 0;
+
+  s_1 = parse_symbol (str);
+  if (!s_1)
+    return (NULL);
+
+  s_2 = parse_symbol (s_1);
+
+  // If no second <|> is found, return everything from first <|> to the end.
+  if (!s_2)
+    {
+      len = strlen (s_1);
+      ret = emalloc (len);
+      strncpy (ret, s_1, len - 1);
+    }
+  else
+    {
+      /** @TODO Instead of modifying s2, length for strncpy could be calculated
+       * like (s_2 - s_1). */
+      int c;
+      s_2 = s_2 - 4;
+      c = s_2[0];
+      s_2[0] = 0;
+      len = strlen (s_1);
+      ret = emalloc (len);
+      strncpy (ret, s_1, len - 1);
+      s_2[0] = c;
+    }
+
 #ifdef DEBUGMORE
- fprintf(stderr, "%s:%d got %s returning \"%s\"\n", __FILE__, __LINE__, str, ret);
+  fprintf (stderr, "%s:%d got %s returning \"%s\"\n", __FILE__, __LINE__, str, ret);
 #endif
-  return(ret);
+
+ return ret;
 }
- 
- 
