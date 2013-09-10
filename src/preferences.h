4787acd194b501323a0169ff8ed19705eee08278
Jan-Oliver Wagner <jan-oliver.wagner@greenbone.net>
2013-09-10 14:19:37 +0000

    * src/preferences.h: Remove yet another two forgotten protos.
    git-svn-id: svn+ssh://scm.wald.intevation.org/openvas/trunk/openvas-scanner@17613 423fd1db-d629-0410-8442-d21db03e70f4

--- src/preferences.h
+++ src/preferences.h
@@ -23,8 +23,6 @@
 * You should have received a copy of the GNU General Public License
 * along with this program; if not, write to the Free Software
 * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
-*
-*
 */
 
 
@@ -32,7 +30,6 @@
 #define __OPENVAS_PREFERENCES_H
 
 int preferences_init (char *, struct arglist **);
-int preferences_new (char *);
 int preferences_process (char *, struct arglist *);
 int preferences_get_checks_read_timeout (struct arglist *);
 int preferences_log_whole_attack (struct arglist *);
@@ -46,7 +43,6 @@ char *preferences_get_string (struct arglist *, char *);
 int preferences_autoload_dependencies (struct arglist *);
 int preferences_safe_checks_enabled (struct arglist *);
 
-int preferences_delay_between_scans (struct arglist *);
 void preferences_reset_cache ();
 int preferences_nasl_no_signature_check (struct arglist *);
 int preferences_drop_privileges (struct arglist *);
