8767bd523ad57414b6f184e5dd65502f5a5538fb
Michael Wiegand <michael.wiegand@greenbone.net>
2014-08-29 13:21:13 +0000

    * CMakeLists.txt, src/CMakeLists.txt, src/otp.c, src/otp.h,   tools/greenbone-nvt-sync: Ensure the GPLv2+ license notice uses the   paragraph suggested by the Free Software Foundation.
    git-svn-id: svn+ssh://scm.wald.intevation.org/openvas/trunk/openvas-scanner@20207 423fd1db-d629-0410-8442-d21db03e70f4

--- src/otp.c
+++ src/otp.c
@@ -8,9 +8,10 @@
 * Copyright:
 * Copyright (C) 2009 Greenbone Networks GmbH
 *
-* This program is free software; you can redistribute it and/or modify
-* it under the terms of the GNU General Public License version 2 or later,
-* as published by the Free Software Foundation
+* This program is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public License
+* as published by the Free Software Foundation; either version 2
+* of the License, or (at your option) any later version.
 *
 * This program is distributed in the hope that it will be useful,
 * but WITHOUT ANY WARRANTY; without even the implied warranty of
