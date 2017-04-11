8d88c9b42f7f7fc41106ee1f5e03217f1b18c7e0
Juan Nicola <juan.nicola@greenbone.net>
2017-04-11 09:11:05 +0000

    * nasl/lint.c: New GSList dont_check_list. (stringcompare): New Function. (nasl_lint_call): Add case CONST_DATA to create a list of functions checked with the nasl function defined_func(). Listed function will not be checked by lint. (nasl_lint): New variable defined_flag.
    git-svn-id: svn+ssh://scm.wald.intevation.org/openvas/trunk/openvas-scanner@28219 423fd1db-d629-0410-8442-d21db03e70f4

--- nasl/lint.c
+++ nasl/lint.c
@@ -32,6 +32,14 @@
 #define NASL_DEBUG 0
 #endif
 
+#undef G_LOG_DOMAIN
+/**
+ * @brief GLib logging domain.
+ */
+#define G_LOG_DOMAIN "lib  nasl"
+
+GSList* dont_check_list = NULL;
+
 tree_cell *
 nasl_lint_def (lex_ctxt * lexic, tree_cell * st, int lint_mode)
 {
@@ -41,6 +49,7 @@ nasl_lint_def (lex_ctxt * lexic, tree_cell * st, int lint_mode)
   switch (st->type)
     {
     case NODE_FUN_DEF:
+
       if (lint_mode == 0)
         {
           if (decl_nasl_func (lexic, st, lint_mode) == NULL)
@@ -61,28 +70,50 @@ nasl_lint_def (lex_ctxt * lexic, tree_cell * st, int lint_mode)
 }
 
 
+int stringcompare (char *list_data_a, char *list_data_b)
+{
+  return g_strcmp0 (list_data_a, list_data_b);
+}
+
+
 tree_cell *
-nasl_lint_call (lex_ctxt * lexic, tree_cell * st)
+nasl_lint_call (lex_ctxt * lexic, tree_cell * st, int *defined_flag)
 {
   int i;
   tree_cell *ret = FAKE_CELL;
   nasl_func *pf;
 
-
   switch (st->type)
     {
+    case CONST_DATA:
+      if (st->x.str_val != NULL && *defined_flag == 1)
+        {
+          dont_check_list = g_slist_append (dont_check_list,
+                                            (char*)(st->x.str_val));
+          *defined_flag = 0;
+          return FAKE_CELL;
+        }
+      return NULL;
+
     case NODE_FUN_CALL:
+      if (st->x.str_val != NULL)
+          if (g_slist_find_custom (dont_check_list, st->x.str_val,
+                                   (GCompareFunc)stringcompare))
+            return FAKE_CELL;
+
       pf = get_func_ref_by_name (lexic, st->x.str_val);
       if (pf == NULL)
         {
           nasl_perror (lexic, "Undefined function '%s'\n", st->x.str_val);
           return NULL;
         }
+      if (g_strcmp0 (st->x.str_val, "defined_func") == 0)
+        *defined_flag = 1;
 
     default:
       for (i = 0; i < 4; i++)
         if (st->link[i] != NULL && st->link[i] != FAKE_CELL)
-          if ((ret = nasl_lint_call (lexic, st->link[i])) == NULL)
+          if ((ret = nasl_lint_call (lexic, st->link[i], defined_flag)) == NULL)
             return NULL;
       return ret;
     }
@@ -92,31 +123,33 @@ nasl_lint_call (lex_ctxt * lexic, tree_cell * st)
 tree_cell *
 nasl_lint (lex_ctxt * lexic, tree_cell * st)
 {
-
   lex_ctxt * lexic_aux;
   tree_cell *ret = FAKE_CELL;
   int lint_mode = 1;
+  int defined_flag = 0;
 
   lexic_aux = g_memdup (lexic, sizeof(struct struct_lex_ctxt));
-  /* first loads all defined functions*/
 
+  /* first loads all defined functions*/
   if ((ret = nasl_lint_def (lexic_aux, st, lint_mode)) == NULL)
     {
       g_free (lexic_aux);
       return ret;
     }
-  /* check if a called function was defined */
 
-  if ((ret = nasl_lint_call (lexic_aux, st)) == NULL)
-     {
+  /* check if a called function was defined */
+  if ((ret = nasl_lint_call (lexic_aux, st, &defined_flag)) == NULL)
+    {
+      g_slist_free (dont_check_list);
       g_free (lexic_aux);
       return ret;
-     }
+    }
 
   /* now check that each function was loaded just once */
   lint_mode = 0;
   nasl_lint_def (lexic, st, lint_mode);
 
+  g_slist_free (dont_check_list);
   g_free (lexic_aux);
   return ret;
 }
