17221615fa914a71061c28c4ab8378d901b603fa
Juan Nicola <juan.nicola@greenbone.net>
2017-06-21 15:25:46 +0000

    * nasl/nasl_signature.c (examine_signatures): Add argument. 	Remove while loop because analyze only one signature. 	Remove not needed variables. Update documentation. 	(nasl_verify_signature): Add variables. Load file contents into 	buffers. Use gpgme_data_new_from_mem() instead of 	gpgme_data_new_from_file(). Search manually the signature inside the 	file. Update documentation.
    git-svn-id: svn+ssh://scm.wald.intevation.org/openvas/trunk/openvas-scanner@28765 423fd1db-d629-0410-8442-d21db03e70f4

--- nasl/nasl_signature.c
+++ nasl/nasl_signature.c
@@ -48,59 +48,52 @@ print_gpgme_error (char *function, gpgme_error_t err)
 }
 
 /**
- * @brief Checks whether the signature verification result contains at least one
- * @brief signature and whether all signatures are fully valid.
+ * @brief Checks whether the signature verification result contains one
+ * @brief signature and whether signature is fully valid.
  *
- * The function returns 1 if all signatures are fully valid and 0 otherwise.
+ * The function returns 1 if the signature is fully valid and 0 otherwise.
  *
  * @param result The verification result to examine.
  *
- * @return 1 if signatures found and all are fully valid, 0 otherwise.
+ * @return 1 if the signature is found and it is fully valid, 0 otherwise.
  */
 static int
-examine_signatures (gpgme_verify_result_t result)
+examine_signatures (gpgme_verify_result_t result, int sig_count)
 {
-  int num_sigs = 0;
-  int num_valid = 0;
   gpgme_signature_t sig;
 
   nasl_trace (NULL, "examine_signatures\n");
 
   sig = result->signatures;
-  while (sig)
+
+  if (nasl_trace_enabled ())
     {
-      num_sigs += 1;
-
-      if (nasl_trace_enabled ())
-        {
-          nasl_trace (NULL, "examine_signatures: signature #%d:\n", num_sigs);
-          nasl_trace (NULL, "examine_signatures:    summary: %d\n",
-                      sig->summary);
-          nasl_trace (NULL, "examine_signatures:    validity: %d\n",
-                      sig->validity);
-          nasl_trace (NULL, "examine_signatures:    status: %s\n",
-                      gpg_strerror (sig->status));
-          nasl_trace (NULL, "examine_signatures:    timestamp: %ld\n",
-                      sig->timestamp);
-          nasl_trace (NULL, "examine_signatures:    exp_timestamp: %ld\n",
-                      sig->exp_timestamp);
-          nasl_trace (NULL, "examine_signatures:    fpr: %s\n", sig->fpr);
-        }
-
-      if (sig->summary & GPGME_SIGSUM_VALID)
-        {
-          nasl_trace (NULL, "examine_signatures: signature is valid\n");
-          num_valid += 1;
-        }
-      else
-        {
-          nasl_trace (NULL, "examine_signatures: signature is invalid\n");
-          /** @todo Early stop might be possible. Can return here. */
-        }
-      sig = sig->next;
+      nasl_trace (NULL, "examine_signatures: signature #%d:\n", sig_count);
+      nasl_trace (NULL, "examine_signatures:    summary: %d\n",
+                  sig->summary);
+      nasl_trace (NULL, "examine_signatures:    validity: %d\n",
+                  sig->validity);
+      nasl_trace (NULL, "examine_signatures:    status: %s\n",
+                  gpg_strerror (sig->status));
+      nasl_trace (NULL, "examine_signatures:    timestamp: %ld\n",
+                  sig->timestamp);
+      nasl_trace (NULL, "examine_signatures:    exp_timestamp: %ld\n",
+                  sig->exp_timestamp);
+      nasl_trace (NULL, "examine_signatures:    fpr: %s\n", sig->fpr);
     }
 
-  return num_sigs > 0 && num_sigs == num_valid;
+  if (sig->summary & GPGME_SIGSUM_VALID)
+    {
+      nasl_trace (NULL, "examine_signatures: signature is valid\n");
+      return 1;
+    }
+  else
+    {
+      nasl_trace (NULL, "examine_signatures: signature is invalid\n");
+      /** @todo Early stop might be possible. Can return here. */
+    }
+
+  return 0;
 }
 
 
@@ -109,82 +102,130 @@ examine_signatures (gpgme_verify_result_t result)
  * The name of the signature file is derived from FILENAME by appending
  * ".asc".
  *
- * If a signature file exists and it contains only fully valid
- * signatures, the function returns 0.  If any of the signatures is not
- * valid or was made by an unknown or untrusted key, the function
+ * If a signature file exists and it contains at least one fully valid
+ * signature, the function returns 0.  If all of the signatures are not
+ * valid or were made by an unknown or untrusted key, the function
  * returns 1.  If an error occurs or the file does not have a
  * corresponding detached signature the function returns -1.
  *
  * @param filename Filename (e.g. 1.txt) for which to check signature (e.g.
                    1.txt.asc).
  *
- * @return Zero, if files exists and all signatures are fully trusted. 1 if at
- *         least one signature from invalid or untrusted key. -1 on missing file
- *         or error.
+ * @return Zero, if files exists and at least one signature is fully trusted.
+ *         1 if all signatures are invalid or untrusted key.
+ *         -1 on missing file or error.
  */
 int
 nasl_verify_signature (const char *filename)
 {
-  int retcode = -1;
+  int retcode = -1, sig_count = 0;
   char *sigfilename = NULL;
+  gsize siglen, flen;
+  gchar * scontent = NULL;
+  gchar * offset = NULL;
+  gchar * endpos = NULL;
+  gchar * fcontent = NULL;
   gchar * path = g_build_filename (OPENVAS_SYSCONF_DIR, "gnupg", NULL);
+  gboolean success;
   gpgme_error_t err;
   gpgme_ctx_t ctx = gvm_init_gpgme_ctx_from_dir (path);
   gpgme_data_t sig = NULL, text = NULL;
 
   g_free (path);
-
   if (ctx == NULL)
     {
       nasl_trace (NULL, "gpgme context could not be initialized.\n");
       goto fail;
     }
 
+  /* Scriptfile is buffered. */
   nasl_trace (NULL, "nasl_verify_signature: loading scriptfile '%s'\n",
-              filename);
+                  filename);
+  if (!g_file_get_contents (filename, &fcontent, &flen, NULL))
+    goto fail;
 
-  err = gpgme_data_new_from_file (&text, filename, 1);
-  if (err)
-    {
-      print_gpgme_error ("gpgme_data_new_from_file", err);
-      goto fail;
-    }
-
+  /* Signatures file is buffered. */
   sigfilename = g_malloc0 (strlen (filename) + 4 + 1);
   strcpy (sigfilename, filename);
   strcat (sigfilename, ".asc");
   nasl_trace (NULL, "nasl_verify_signature: loading signature file '%s'\n",
               sigfilename);
-  err = gpgme_data_new_from_file (&sig, sigfilename, 1);
-  if (err)
+
+  success = g_file_get_contents (sigfilename, &scontent, NULL, NULL);
+  /* If the signature file doesn't exist, fail without an error message
+   * because an unsigned file is a very common and expected
+   * condition */
+  if (!success)
+    goto fail;
+
+  /* Start to parse the signature file to find signatures. */
+  offset = g_strstr_len (scontent, strlen(scontent), "-----B");
+  if (!offset)
     {
-      /* If the file doesn't exist, fail without an error message
-       * because an unsigned file is a very common and expected
-       * condition */
-      if (gpgme_err_code (err) != GPG_ERR_ENOENT)
-        print_gpgme_error ("gpgme_data_new_from_file", err);
+      nasl_trace (NULL, "nasl_verify_signature: No signature in '%s'\n",
+                  sigfilename);
+      goto fail;
+    }
+  endpos = g_strstr_len (offset,-1, "-----E");
+  if (endpos)
+    siglen = strlen(offset) - strlen(endpos) + 17 ;
+
+
+  do
+    {
+      sig_count++;
+
+      /* Load file in memory. */
+      err = gpgme_data_new_from_mem (&text, fcontent, flen, 1);
+      if (err)
+        {
+          print_gpgme_error ("gpgme_data_new_from_file", err);
+          goto fail;
+        }
+
+      /* Load a founded signature in memory. */
+      err = gpgme_data_new_from_mem (&sig, offset, siglen, 1);
+      if (err)
+        nasl_trace (NULL, "nasl_verify_signature: %s: %s\n",
+                    sigfilename, gpgme_strerror (err));
+
+      /* Verify the signature. */
+      err = gpgme_op_verify (ctx, sig, text, NULL);
+      nasl_trace (NULL, "nasl_verify_signature: gpgme_op_verify "
+                  "-> '%d'\n", err);
+      if (err)
+          print_gpgme_error ("gpgme_op_verify", err);
       else
-        nasl_trace (NULL, "nasl_verify_signature: %s: %s\n", sigfilename,
-                    gpgme_strerror (err));
-      goto fail;
-    }
+        {
+          if (examine_signatures (gpgme_op_verify_result (ctx), sig_count))
+            {
+              retcode = 0;
+              goto fail;
+            }
+          else
+            retcode = 1;
+        }
 
-  err = gpgme_op_verify (ctx, sig, text, NULL);
-  nasl_trace (NULL, "nasl_verify_signature: gpgme_op_verify -> '%d'\n", err);
-  if (err)
-    {
-      print_gpgme_error ("gpgme_op_verify", err);
-      goto fail;
-    }
+      /* Search a new signature. */
+      offset = g_strstr_len (offset + 1, strlen(offset), "-----B");
+      if (offset)
+        if ( (endpos = g_strstr_len (offset, strlen (offset), "-----E")) )
+          siglen = (strlen(offset) - strlen(endpos) + 17);
 
-  if (examine_signatures (gpgme_op_verify_result (ctx)))
-    retcode = 0;
-  else
-    retcode = 1;
+      gpgme_data_release (sig);
+      sig = NULL;
+      gpgme_data_release (text);
+      text = NULL;
 
-fail:
-  gpgme_data_release (sig);
-  gpgme_data_release (text);
+    } while (offset);
+
+ fail:
+  g_free (scontent);
+  g_free (fcontent);
+  if (sig)
+    gpgme_data_release (sig);
+  if (text)
+    gpgme_data_release (text);
   if (ctx != NULL)
     gpgme_release (ctx);
   g_free (sigfilename);
