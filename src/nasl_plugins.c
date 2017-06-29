5808779de517738f46166c8b37d67310d8c701eb
Juan Nicola <juan.nicola@greenbone.net>
2017-06-29 06:52:09 +0000

    Backport r28861. * src/nasl_plugins.c (nasl_plugin_add): Set the preference's name to Timeout instead of Script timeout.
    git-svn-id: svn+ssh://scm.wald.intevation.org/openvas/branches/openvas-scanner-5.1@28862 423fd1db-d629-0410-8442-d21db03e70f4

--- src/nasl_plugins.c
+++ src/nasl_plugins.c
@@ -180,7 +180,7 @@ nasl_plugin_add (char *folder, char *name)
     {
       char *pref, *cname, def_val[5];
       snprintf (def_val, 5, "%d", nvti->timeout);
-      const nvtpref_t *np = nvtpref_new ("Script timeout", "entry", def_val);
+      const nvtpref_t *np = nvtpref_new ("Timeout", "entry", def_val);
 
       cname = g_strdup (nvtpref_name (np));
       g_strchomp (cname);
