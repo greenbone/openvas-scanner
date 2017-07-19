5b616896140e4598236c2996b6383b81198b8be4
Juan Nicola <juan.nicola@greenbone.net>
2017-07-19 11:44:00 +0000

    Check if the included files are used or not.
    * nasl/lint.c (check_called_files, print_uncall_files):New functions.
    (nasl_lint_def): Get hash table as new parameter. Add into the table
    the included files.
    (nasl_lint_call): Get hash table as new parameter. Mark the included
    file in the table, if a called function is inside the file.
    (nasl_lint): New hash table. New single linked list. Pass the table as
    argument to nasl_lint_def() and nasl_lint_call() functions. Check if
    the included files are used or not.
    
    
    git-svn-id: svn+ssh://scm.wald.intevation.org/openvas/trunk/openvas-scanner@29114 423fd1db-d629-0410-8442-d21db03e70f4

--- nasl/lint.c
+++ nasl/lint.c
@@ -38,11 +38,35 @@
  */
 #define G_LOG_DOMAIN "lib  nasl"
 
+
+/* It adds to a list the inc files, which have never been called. */
+void
+check_called_files (gpointer key, gpointer value, GSList **unusedfiles)
+{
+  if (key != NULL)
+    if (!g_strcmp0 (value,("NO")))
+      *unusedfiles = g_slist_append(*unusedfiles, key);
+}
+
+/* It lookups into a list for never called files, and it shows a msg. */
+void
+print_uncall_files (gpointer filename, gpointer lexic)
+{
+  if (filename != NULL)
+    {
+      nasl_perror (lexic, "The included file '%s' is never used.",
+                   (char*)filename);
+      lexic = NULL;
+    }
+}
+
 tree_cell *
-nasl_lint_def (lex_ctxt * lexic, tree_cell * st, int lint_mode)
+nasl_lint_def (lex_ctxt * lexic, tree_cell * st,
+               int lint_mode, GHashTable **include_files)
 {
   int i;
   tree_cell *ret = FAKE_CELL;
+  const char *incname = NULL;
 
   switch (st->type)
     {
@@ -57,23 +81,29 @@ nasl_lint_def (lex_ctxt * lexic, tree_cell * st, int lint_mode)
 
       /* x.str_val = function name, [0] = argdecl, [1] = block */
       decl_nasl_func (lexic, st, lint_mode);
+
+      incname = nasl_get_filename (st->x.str_val);
+      g_hash_table_replace (*include_files, g_strdup (incname), g_strdup("NO"));
       /* fallthrough */
 
     default:
       for (i = 0; i < 4; i++)
         if (st->link[i] != NULL && st->link[i] != FAKE_CELL)
-          if ((ret = nasl_lint_def (lexic, st->link[i], lint_mode)) == NULL)
+          if ((ret = nasl_lint_def (lexic, st->link[i], lint_mode,
+                                    include_files)) == NULL)
             return NULL;
       return ret;
     }
 }
 
 tree_cell *
-nasl_lint_call (lex_ctxt * lexic, tree_cell * st, int *defined_flag)
+nasl_lint_call (lex_ctxt * lexic, tree_cell * st, int *defined_flag,
+                GHashTable **include_files)
 {
   int i;
   tree_cell *ret = FAKE_CELL;
   nasl_func *pf;
+  const char *incname = NULL;
 
   switch (st->type)
     {
@@ -94,6 +124,17 @@ nasl_lint_call (lex_ctxt * lexic, tree_cell * st, int *defined_flag)
           nasl_perror (lexic, "Undefined function '%s'\n", st->x.str_val);
           return NULL;
         }
+
+      if (*include_files && st->x.str_val)
+       {
+         if (g_hash_table_lookup (*include_files,
+                                  g_strdup (nasl_get_filename (st->x.str_val))))
+           {
+             incname = nasl_get_filename (st->x.str_val);
+             g_hash_table_replace (*include_files, g_strdup (incname),
+                                   g_strdup("YES"));
+           }
+       }
       if (g_strcmp0 (st->x.str_val, "defined_func") == 0)
         *defined_flag = 1;
       /* fallthrough */
@@ -101,12 +142,12 @@ nasl_lint_call (lex_ctxt * lexic, tree_cell * st, int *defined_flag)
     default:
       for (i = 0; i < 4; i++)
         if (st->link[i] != NULL && st->link[i] != FAKE_CELL)
-          if ((ret = nasl_lint_call (lexic, st->link[i], defined_flag)) == NULL)
+          if ((ret = nasl_lint_call (lexic, st->link[i], defined_flag,
+                                     include_files)) == NULL)
             return NULL;
       return ret;
     }
 }
-
 
 tree_cell *
 nasl_lint (lex_ctxt * lexic, tree_cell * st)
@@ -115,28 +156,49 @@ nasl_lint (lex_ctxt * lexic, tree_cell * st)
   tree_cell *ret = FAKE_CELL;
   int lint_mode = 1;
   int defined_flag = 0;
+  GHashTable *include_files = NULL;
+  GSList *unusedfiles = NULL;
+
+  include_files = g_hash_table_new_full
+    (g_str_hash, g_str_equal, g_free, g_free);
 
   lexic_aux = init_empty_lex_ctxt ();
   lexic_aux->script_infos = lexic->script_infos;
   lexic_aux->oid = lexic->oid;
 
-  /* first loads all defined functions*/
-  if ((ret = nasl_lint_def (lexic_aux, st, lint_mode)) == NULL)
+  /* First loads all defined functions. */
+  if ((ret = nasl_lint_def (lexic_aux, st, lint_mode, &include_files)) == NULL)
     {
       free_lex_ctxt (lexic_aux);
       return ret;
     }
 
-  /* check if a called function was defined */
-  if ((ret = nasl_lint_call (lexic_aux, st, &defined_flag)) == NULL)
+  /* Check if a called function was defined. */
+  if ((ret = nasl_lint_call (lexic_aux, st, &defined_flag,
+                             &include_files)) == NULL)
     {
       free_lex_ctxt (lexic_aux);
       return ret;
     }
 
-  /* now check that each function was loaded just once */
+  /* Check if the included files are used or not. */
+  g_hash_table_foreach (include_files, (GHFunc)check_called_files,
+                        &unusedfiles);
+  if (unusedfiles != NULL)
+    g_slist_foreach (unusedfiles, (GFunc)print_uncall_files, lexic);
+  if ((g_slist_length (unusedfiles)) > 0)
+    {
+      g_hash_table_remove_all (include_files);
+      g_slist_free (unusedfiles);
+      free_lex_ctxt (lexic_aux);
+      return NULL;
+    }
+  g_hash_table_remove_all (include_files);
+  g_slist_free (unusedfiles);
+
+  /* Now check that each function was loaded just once. */
   lint_mode = 0;
-  nasl_lint_def (lexic, st, lint_mode);
+  nasl_lint_def (lexic, st, lint_mode, &include_files);
 
   free_lex_ctxt (lexic_aux);
   return ret;
