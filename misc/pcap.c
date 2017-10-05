4eaf3f8b04894b70e7aabcf01b29da6e6f20c94d
Hani Benhabiles <hani.benhabiles@greenbone.net>
2017-10-05 00:09:38 +0000

    * misc/pcap.c (get_random_bytes): Remove function. (v6_getsourceip): Use rand() instead of get_random_bytes().
    git-svn-id: svn+ssh://scm.wald.intevation.org/openvas/trunk/openvas-scanner@29746 423fd1db-d629-0410-8442-d21db03e70f4

--- misc/pcap.c
+++ misc/pcap.c
@@ -485,73 +485,6 @@ get_datalink_size (int datalink)
   return (offset);
 }
 
-int
-get_random_bytes (void *buf, int numbytes)
-{
-  static char bytebuf[2048];
-  static char badrandomwarning = 0;
-  static int bytesleft = 0;
-  int res;
-  int tmp;
-  struct timeval tv;
-  FILE *fp = NULL;
-  short *iptr;
-
-  if (numbytes < 0 || numbytes > 0xFFFF)
-    return -1;
-
-  if (bytesleft == 0)
-    {
-      fp = fopen ("/dev/urandom", "r");
-      if (!fp)
-        fp = fopen ("/dev/random", "r");
-      if (fp)
-        {
-          res = fread (bytebuf, 1, sizeof (bytebuf), fp);
-          if (res != sizeof (bytebuf))
-            {
-              fclose (fp);
-              fp = NULL;
-            }
-          bytesleft = sizeof (bytebuf);
-        }
-      if (!fp)
-        {
-          unsigned int i;
-
-          if (badrandomwarning == 0)
-            {
-              badrandomwarning++;
-            }
-          /* Seed our random generator */
-          gettimeofday (&tv, NULL);
-          srand ((tv.tv_sec ^ tv.tv_usec) ^ getpid ());
-
-          for (i = 0; i < sizeof (bytebuf) / sizeof (short); i++)
-            {
-              iptr = (short *) ((char *) bytebuf + i * sizeof (short));
-              *iptr = rand ();
-            }
-          bytesleft = (sizeof (bytebuf) / sizeof (short)) * sizeof (short);
-          /*    ^^^^^^^^^^^^^^^not as meaningless as it looks  */
-        }
-      else
-        fclose (fp);
-    }
-  if (numbytes <= bytesleft)
-    {                           /* we can cover it */
-      memcpy (buf, bytebuf + (sizeof (bytebuf) - bytesleft), numbytes);
-      bytesleft -= numbytes;
-      return 0;
-    }
-
-  /* We don't have enough */
-  memcpy (buf, bytebuf + (sizeof (bytebuf) - bytesleft), bytesleft);
-  tmp = bytesleft;
-  bytesleft = 0;
-  return get_random_bytes ((char *) buf + tmp, numbytes - tmp);
-}
-
 struct interface_info *
 v6_getinterfaces (int *howmany)
 {
@@ -712,7 +645,7 @@ v6_getsourceip (struct in6_addr *src, struct in6_addr *dst)
   char name[INET6_ADDRSTRLEN];
 #endif
 
-  get_random_bytes (&p1, 2);
+  p1 = (unsigned short) rand ();
   if (p1 < 5000)
     p1 += 5000;
 
