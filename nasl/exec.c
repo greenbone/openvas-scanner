greenbone/openvas-scanner

commit 4b9d3db08042d8196aaeba01574fde4b8f59fb9c

* nasl/exec.c (cell_cmp): Use cell2str() to copy the cell's values since it checks if the cell is NULL and	then are freed conveniently.
git-svn-id: svn+ssh://scm.wald.intevation.org/openvas/trunk/openvas-scanner@29164 423fd1db-d629-0410-8442-d21db03e70f4

nasl/exec.c
@@ -251,8 +251,7 @@ cell_cmp (lex_ctxt * lexic, tree_cell * c1, tree_cell * c2)
   long int x1, x2;
   char *s1, *s2;
   int len_s1, len_s2, len_min;
-  gchar *n1 = c1->x.str_val;
-  gchar *n2 = c2->x.str_val;
+  gchar *n1, *n2;
 
 #if NASL_DEBUG >= 0
   if (c1 == NULL || c1 == FAKE_CELL)
@@ -261,6 +260,9 @@ cell_cmp (lex_ctxt * lexic, tree_cell * c1, tree_cell * c2)
     nasl_perror (lexic, "cell_cmp: c2 == NULL !\n");
 #endif
 
+  n1 = cell2str (lexic, c1);
+  n2 = cell2str (lexic, c2);
+  
   /* We first convert the cell to atomic types. */
   c1 = cell2atom (lexic, c1);
   c2 = cell2atom (lexic, c2);
@@ -284,6 +286,8 @@ cell_cmp (lex_ctxt * lexic, tree_cell * c1, tree_cell * c2)
     {
       deref_cell (c1);
       deref_cell (c2);
+      g_free (n1);
+      g_free (n2);
       return 0;
     }
 
@@ -308,6 +312,8 @@ cell_cmp (lex_ctxt * lexic, tree_cell * c1, tree_cell * c2)
       {
         deref_cell (c1);
         deref_cell (c2);
+        g_free (n1);
+        g_free (n2);
         return -1;              /* NULL is smaller than anything else */
       }
   else if (typ2 == 0)           /* 2nd argument is null */
@@ -317,6 +323,8 @@ cell_cmp (lex_ctxt * lexic, tree_cell * c1, tree_cell * c2)
       {
         deref_cell (c1);
         deref_cell (c2);
+        g_free (n1);
+        g_free (n2);
         return 1;               /* Anything else is greater than NULL  */
       }
   else
@@ -326,8 +334,13 @@ cell_cmp (lex_ctxt * lexic, tree_cell * c1, tree_cell * c2)
                    n1, nasl_type_name (typ1), n2, nasl_type_name (typ2));
       deref_cell (c1);
       deref_cell (c2);
+      g_free (n1);
+      g_free (n2);
       return 0;
     }
+  g_free (n1);
+  g_free (n2);
+
 
   switch (typ)
     {

commit 642c50ee5de3c7d370d52d986a10128cb037d324

* nasl/exec.c (cell_cmp): Fix regression introduced in r29164. Don't call cell2str() before cell2atom() as it leads to executing expressions such as incrementations and function calls twice.
git-svn-id: svn+ssh://scm.wald.intevation.org/openvas/trunk/openvas-scanner@30032 423fd1db-d629-0410-8442-d21db03e70f4

nasl/exec.c
@@ -251,7 +251,6 @@ cell_cmp (lex_ctxt * lexic, tree_cell * c1, tree_cell * c2)
   long int x1, x2;
   char *s1, *s2;
   int len_s1, len_s2, len_min;
-  gchar *n1, *n2;
 
 #if NASL_DEBUG >= 0
   if (c1 == NULL || c1 == FAKE_CELL)
@@ -260,9 +259,6 @@ cell_cmp (lex_ctxt * lexic, tree_cell * c1, tree_cell * c2)
     nasl_perror (lexic, "cell_cmp: c2 == NULL !\n");
 #endif
 
-  n1 = cell2str (lexic, c1);
-  n2 = cell2str (lexic, c2);
-  
   /* We first convert the cell to atomic types. */
   c1 = cell2atom (lexic, c1);
   c2 = cell2atom (lexic, c2);
@@ -286,8 +282,6 @@ cell_cmp (lex_ctxt * lexic, tree_cell * c1, tree_cell * c2)
     {
       deref_cell (c1);
       deref_cell (c2);
-      g_free (n1);
-      g_free (n2);
       return 0;
     }
 
@@ -312,8 +306,6 @@ cell_cmp (lex_ctxt * lexic, tree_cell * c1, tree_cell * c2)
       {
         deref_cell (c1);
         deref_cell (c2);
-        g_free (n1);
-        g_free (n2);
         return -1;              /* NULL is smaller than anything else */
       }
   else if (typ2 == 0)           /* 2nd argument is null */
@@ -323,23 +315,23 @@ cell_cmp (lex_ctxt * lexic, tree_cell * c1, tree_cell * c2)
       {
         deref_cell (c1);
         deref_cell (c2);
-        g_free (n1);
-        g_free (n2);
         return 1;               /* Anything else is greater than NULL  */
       }
   else
     {
+      gchar *n1, *n2;
+
+      n1 = cell2str (lexic, c1);
+      n2 = cell2str (lexic, c2);
       nasl_perror (lexic, "cell_cmp: comparing '%s' of type %s and '%s' of "
                    "type %s does not make sense\n",
                    n1, nasl_type_name (typ1), n2, nasl_type_name (typ2));
-      deref_cell (c1);
-      deref_cell (c2);
       g_free (n1);
       g_free (n2);
+      deref_cell (c1);
+      deref_cell (c2);
       return 0;
     }
-  g_free (n1);
-  g_free (n2);
 
 
   switch (typ)