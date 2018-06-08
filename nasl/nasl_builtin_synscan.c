352624447c11ee57ee42023b5afd81a78eae3ad3
Hani Benhabiles <kroosec@gmail.com>
2018-06-08 11:50:48 +0100

    Fix coverity issue #291981.
    Resource leak.

--- nasl/nasl_builtin_synscan.c
+++ nasl/nasl_builtin_synscan.c
@@ -738,7 +738,10 @@ scan (struct script_infos * env, char* portrange, struct in6_addr *dst6,
   else
     bpf = v6_openbpf (dst6, &src6, magic);
   if (bpf < 0)
-    return -1;
+    {
+      close (soc);
+      return -1;
+    }
   skip = get_datalink_size (bpf_datalink (bpf));
 
   /** This will send packets to ports not in ports list, will it? */
