07c15deceda2de3a42921fdc0424ca1e4bbd615c
Hani Benhabiles <kroosec@gmail.com>
2018-06-08 02:21:28 +0100

    Fix coverity issue #274931.
    Check function return value.

--- nasl/nasl_socket.c
+++ nasl/nasl_socket.c
@@ -587,7 +587,8 @@ nasl_open_sock_udp (lex_ctxt * lexic)
       if (soc < 0)
         return NULL;
       gvm_source_set_socket (soc, 0, AF_INET6);
-      connect (soc, (struct sockaddr *) &soca6, sizeof (soca6));
+      if (connect (soc, (struct sockaddr *) &soca6, sizeof (soca6)) < 0)
+        return NULL;
     }
 
   if (soc > 0 && lowest_socket == 0)
