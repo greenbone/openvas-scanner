224f6bc319a2ae3ff3fdadd90a047704f3c570ed
Jan-Oliver Wagner <jan-oliver.wagner@greenbone.net>
2018-09-25 21:51:12 +0200

    Remove invalid email addresses from headers.
    Those addresses are invalid since many years.
    Also make formatting a bit more consistent.

--- src/plugs_req.h
+++ src/plugs_req.h
@@ -2,10 +2,11 @@
 * $Id$
 * Description: plugs_req.c header.
 *
-* Authors: - Renaud Deraison <deraison@nessus.org> (Original pre-fork develoment)
-*          - Tim Brown <mailto:timb@openvas.org> (Initial fork)
-*          - Laban Mwangi <mailto:labanm@openvas.org> (Renaming work)
-*          - Tarik El-Yassem <mailto:tarik@openvas.org> (Headers section)
+* Authors: 
+* Renaud Deraison <deraison@nessus.org> (Original pre-fork develoment)
+* Tim Brown (Initial fork)
+* Laban Mwangi (Renaming work)
+* Tarik El-Yassem (Headers section)
 *
 * Copyright:
 * Portions Copyright (C) 2006 Software in the Public Interest, Inc.
@@ -23,10 +24,7 @@
 * You should have received a copy of the GNU General Public License
 * along with this program; if not, write to the Free Software
 * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
-*
-*
 */
-
 
 #ifndef PLUGINS_REQUIREMENTS_H__
 #define PLUGINS_REQUIREMENTS_H__
