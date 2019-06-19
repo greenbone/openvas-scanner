d12adfde604fbfcb5cafacd31cb7a1d79069f857
Matt Mundell <matthew.mundell@greenbone.net>
2019-06-19 11:50:48 +0200

    Move main to separate file so tests can include openvas.c

---
+++ src/main.c
@@ -0,0 +1,41 @@
+/* Copyright (C) 2009-2018 Greenbone Networks GmbH
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+/**
+ * @file  main.c
+ * @brief Main function of openvas.
+ *
+ * This file separates out the "main" function of openvas.
+ */
+
+#include "openvas.h"
+
+/**
+ * @brief Main function.
+ *
+ * @param[in]  argc  The number of arguments in argv.
+ * @param[in]  argv  The list of arguments to the program.
+ *
+ * @return EXIT_SUCCESS on success, EXIT_FAILURE on failure.
+ */
+int
+main (int argc, char **argv)
+{
+  return openvas (argc, argv);
+}
