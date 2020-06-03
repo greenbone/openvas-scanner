e93363dc3ebf837502d330971eabcea9b22df1e5
ArnoStiefvater <arno.stiefvater@greenbone.net>
2020-06-03 15:31:27 +0200

    Use alive detection module included in gvm-libs
    Alive detection module Boreas was moved to
    gvm-libs. Tests were moved as well.

--- misc/pcap_tests.c
+++ misc/pcap_tests.c
@@ -16,11 +16,11 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
  */
-#include "../src/alivedetection.h"
 #include "pcap.c"
 
 #include <cgreen/cgreen.h>
 #include <cgreen/mocks.h>
+#include <gvm/boreas/alivedetection.h>
 
 Describe (alivedetection);
 BeforeEach (alivedetection)
