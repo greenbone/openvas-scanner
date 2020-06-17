8d05f21f83be723f6ceb14c7f3910dc766cb361e
ArnoStiefvater <arno.stiefvater@greenbone.net>
2020-06-17 14:42:37 +0200

    Update version command

--- nasl/nasl.c
+++ nasl/nasl.c
@@ -243,7 +243,7 @@ main (int argc, char **argv)
       else
         putchar ('\n');
       printf ("Copyright (C) 2002 - 2004 Tenable Network Security\n");
-      printf ("Copyright (C) 2013 Greenbone Networks GmbH\n\n");
+      printf ("Copyright (C) 2020 Greenbone Networks GmbH\n\n");
       exit (0);
     }
   if (nasl_debug)
