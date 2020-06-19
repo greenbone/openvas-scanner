e48e8cfdddadc8f81070401000275fd77416de08
Juan Jose Nicola <juan.nicola@greenbone.net>
2020-06-19 14:01:07 +0200

    Sleep between checks for last plugin to finish before changing to another category When a plugin is the last one for a plugin's category, it must finish before jumping to the next one. e.g from ACT_ATTACK to ACT_END This sleep prevents to get 100% processor usage during the wait of last plugin's execution.

--- src/attack.c
+++ src/attack.c
@@ -506,6 +506,8 @@ attack_host (struct scan_globals *globals, struct in6_addr *ip, GSList *vhosts,
         }
       else if (plugin == NULL)
         break;
+      else if (plugin != NULL && plugin == PLUG_RUNNING)
+        usleep (50000);
       pluginlaunch_wait_for_free_process (kb);
     }
 
