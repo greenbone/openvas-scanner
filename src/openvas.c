d7b4f081ef1a10c21a03f148a0488fd53de0789b
Juan Jose Nicola <juan.nicola@greenbone.net>
2020-08-31 11:23:42 +0200

    Make the scanner main process the group leader process. Now is independent of the caller process (sudo, ospd-openvas)

--- src/openvas.c
+++ src/openvas.c
@@ -289,6 +289,9 @@ load_scan_preferences (struct scan_globals *globals)
 static void
 scanner_thread (struct scan_globals *globals)
 {
+  /* Make process a group leader, to make it easier to cleanup forked
+   * processes & their children. */
+  setpgid (0, 0);
   nvticache_reset ();
 
   globals->scan_id = g_strdup (global_scan_id);
