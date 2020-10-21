43c3bdd1101d673af392f1830f0a45556e32e31e
ArnoStiefvater <arno.stiefvater@greenbone.net>
2020-10-21 13:25:28 +0200

    Use internal function name in debug messages too
    nasl_get_function_name only return the function
    of the nasl script in which the current function
    is called.
    Now we also include __fun__ in the debug message.
    When the internal function is not called from
    inside a nasl function the placeholder
    "script_main_function" is used as nasl function
    name.

--- misc/network.c
+++ misc/network.c
@@ -1222,10 +1222,10 @@ read_stream_connection_unbuffered (int fd, void *buf0, int min_len, int max_len)
 
     default:
       if (fp->transport || fp->fd != 0)
-        g_message ("Function %s called from %s: "
+        g_message ("Function %s (calling internal function %s) called from %s: "
                    "Severe bug! Unhandled transport layer %d (fd=%d).",
-                   nasl_get_function_name (), nasl_get_plugin_filename (),
-                   fp->transport, fd);
+                   nasl_get_function_name () ?: "script_main_function",
+                   __func__, nasl_get_plugin_filename (), fp->transport, fd);
       else
         g_message ("read_stream_connection_unbuffered: "
                    "fd=%d is closed",
@@ -1403,10 +1403,10 @@ write_stream_connection4 (int fd, void *buf0, int n, int i_opt)
 
     default:
       if (fp->transport || fp->fd != 0)
-        g_message ("Function %s called from %s: "
+        g_message ("Function %s (calling internal function %s) called from %s: "
                    "Severe bug! Unhandled transport layer %d (fd=%d).",
-                   nasl_get_function_name (), nasl_get_plugin_filename (),
-                   fp->transport, fd);
+                   nasl_get_function_name () ?: "script_main_function",
+                   __func__, nasl_get_plugin_filename (), fp->transport, fd);
       else
         g_message ("read_stream_connection_unbuffered: fd=%d is "
                    "closed",
