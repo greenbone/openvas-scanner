886e9b61b3a48cda117a06ca1a3a969e741100a3
Juan Jose Nicola <juan.nicola@greenbone.net>
2021-05-19 03:01:30 -0500

    Add debug_utils files, to init sentry

---
+++ src/debug_utils.c
@@ -0,0 +1,51 @@
+/* Portions Copyright (C) 2021 Greenbone Networks GmbH
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+/**
+ * @file debug_utils.c
+ * @brief Initialize sentry
+ */
+
+#include "debug_utils.h"
+
+#include <gvm/base/logging.h>
+#include <stdio.h> /* for snprintf */
+#include <stdlib.h>
+
+int
+init_sentry (void)
+{
+  char *sentry_dsn_openvas = NULL;
+  char version[96];
+
+#ifdef OPENVAS_GIT_REVISION
+  snprintf (version, sizeof (version), "openvas@%s%s", OPENVAS_VERSION,
+            OPENVAS_GIT_REVISION);
+#else
+  snprintf (version, sizeof (version), "openvas@%s", OPENVAS_VERSION);
+#endif
+
+  sentry_dsn_openvas = getenv ("SENTRY_DSN_OPENVAS");
+  if (gvm_has_sentry_support () && sentry_dsn_openvas && *sentry_dsn_openvas)
+    {
+      gvm_sentry_init (sentry_dsn_openvas, version);
+      return 1;
+    }
+  return 0;
+}
---
+++ src/debug_utils.h
@@ -0,0 +1,33 @@
+/* Portions Copyright (C) 2021 Greenbone Networks GmbH
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+/**
+ * @file debug_utils.h
+ * @brief debug_utils.c headerfile.
+ */
+
+#ifndef _OPENVAS_DEBUG_UTILS_H
+#define _OPENVAS_DEBUG_UTILS_H
+
+#include <gvm/base/gvm_sentry.h> /* for gvm_sentry_init */
+
+int
+init_sentry (void);
+
+#endif
