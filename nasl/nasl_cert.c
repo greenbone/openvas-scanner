e14e85153aa2cfdca2b7e1106e23d5e5319f80ec
Christoph Kraemer <christoph.kraemer@greenbone.net>
2021-06-18 16:42:06 +0200

    clang format

--- nasl/nasl_cert.c
+++ nasl/nasl_cert.c
@@ -70,9 +70,10 @@
 #define atoi_1(p) (*(p) - '0')
 #define atoi_2(p) ((atoi_1 (p) * 10) + atoi_1 ((p) + 1))
 #define atoi_4(p) ((atoi_2 (p) * 100) + atoi_2 ((p) + 2))
-#define xtoi_1(p)             \
-  (*(p) <= '9' ? (*(p) - '0') \
-               : *(p) <= 'F' ? (*(p) - 'A' + 10) : (*(p) - 'a' + 10))
+#define xtoi_1(p)                    \
+  (*(p) <= '9'   ? (*(p) - '0')      \
+   : *(p) <= 'F' ? (*(p) - 'A' + 10) \
+                 : (*(p) - 'a' + 10))
 #define xtoi_2(p)                              \
   ((xtoi_1 ((const unsigned char *) (p)) * 16) \
    + xtoi_1 ((const unsigned char *) (p) + 1))
