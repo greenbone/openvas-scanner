f6ec9c2e7667aca35706e612d9652ca763e8d0e5
Juan Jose Nicola <juan.nicola@greenbone.net>
2021-08-06 02:11:05 -0500

    Handle ERR_NO_FREE_SLOT separated from ERR_CANT_FORK. Because ERR_CANT_FORK should be used when there are resource issues, and when the system is not being able to fork a new processes.
    ERR_NO_FREE_SLOT is an internal error of the host scan process, which could be in most cases a temporal issue.
    We don't stop the host scan process. We just try again until the process table has a free slot.
    
    (cherry picked from commit ad3bd82b2275d4743a2f59340b0e68b94f02dbd2)

--- src/attack.c
+++ src/attack.c
@@ -471,7 +471,10 @@ launch_plugin (struct scan_globals *globals, struct scheduler_plugin *plugin,
   if (pid < 0)
     {
       plugin->running_state = PLUGIN_STATUS_UNRUN;
-      ret = ERR_CANT_FORK;
+      if (pid == ERR_NO_FREE_SLOT )
+        ret = ERR_NO_FREE_SLOT;
+      else
+        ret = ERR_CANT_FORK;
       goto finish_launch_plugin;
     }
 
@@ -554,13 +557,19 @@ attack_host (struct scan_globals *globals, struct in6_addr *ip, GSList *vhosts,
                   comm_send_status_host_dead (main_kb, ip_str);
                   goto host_died;
                 }
+              else if (e == ERR_NO_FREE_SLOT)
+                {
+                  g_debug ("fork() failed for %s. Not free slot to run a plugin.",
+                           plugin->oid);
+                  goto again;
+                }
               else if (e == ERR_CANT_FORK)
                 {
                   if (forks_retry < MAX_FORK_RETRIES)
                     {
                       forks_retry++;
-                      g_debug ("fork() failed - sleeping %d seconds (%s)",
-                               forks_retry, strerror (errno));
+                      g_debug ("fork() failed for %s - sleeping %d seconds (%s)",
+                               plugin->oid, forks_retry, strerror (errno));
                       fork_sleep (forks_retry);
                       goto again;
                     }
