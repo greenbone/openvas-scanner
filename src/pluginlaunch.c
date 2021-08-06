dd3e46dd6d60c5b57ca7035be3ad12e63e7579a2
Juan Jose Nicola <juan.nicola@greenbone.net>
2021-08-06 01:48:55 -0500

    Check if the max running process was reached or even exceed.
    Fix SC-177
    
    On some cases, the parallel scan is disabled and,
    the max_running_processes variable is set to 1, while the current number of
    running process is grether than 1. Therefore, condition in the while loop are
    not satisfied and never wait for free slot. At the end, the process table is full,
    and this produced the host scan process to stop, because not free slot for a new process.

--- src/pluginlaunch.c
+++ src/pluginlaunch.c
@@ -462,7 +462,7 @@ pluginlaunch_wait_for_free_process (kb_t kb)
   /* Max number of processes are still running, wait for a child to exit or
    * to timeout. */
   while (
-    (num_running_processes == max_running_processes)
+    (num_running_processes >= max_running_processes)
     || (num_running_processes > 0 && (check_memory () || check_sysload ())))
     {
       sigset_t mask;
