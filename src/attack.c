db26ecbdb4526dd8d1f8e7ff9454c205a320a116
Christoph Kraemer <christoph.kraemer@greenbone.net>
2021-09-28 12:05:31 +0200

    use duplicate host function

--- src/attack.c
+++ src/attack.c
@@ -1294,7 +1294,7 @@ attack_network (struct scan_globals *globals)
 
           if (host)
             {
-              gvm_hosts_add (alive_hosts_list, host);
+              gvm_hosts_add (alive_hosts_list, gvm_duplicate_host (host));
             }
           else
             g_debug ("%s: got NULL host, stop/finish scan", __func__);
