ae73614faddbfa68d0a9f0f663ce959d147c04de
Juan Jose Nicola <juan.nicola@greenbone.net>
2021-10-04 02:01:52 -0500

    Add one more test

--- src/preference_handler_tests.c
+++ src/preference_handler_tests.c
@@ -17,6 +17,7 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
+#include "../misc/plugutils.c"
 #include "preference_handler.c"
 
 #include <cgreen/cgreen.h>
@@ -26,6 +27,7 @@
 #include <cgreen/reporter.h>
 #include <cgreen/unit.h>
 #include <gvm/base/prefs.h>     /* for prefs_get() */
+#include <gvm/util/nvticache.h> // for nvticache_initialized
 #include <gvm/util/uuidutils.h> /* gvm_uuid_make */
 #include <json-glib/json-glib.h>
 #include <stdio.h>
@@ -64,6 +66,19 @@ __wrap_store_file (__attribute__ ((unused)) struct scan_globals *globals,
   return 0;
 }
 
+__attribute__ ((weak)) int
+__real_nvticache_initialized (void);
+bool nvticache_initialized_real = true;
+
+int
+__wrap_nvticache_initialized ()
+{
+  if (nvticache_initialized_real == true)
+    return __real_nvticache_initialized ();
+
+  return 1;
+}
+
 ///* Wrap calloc */
 __attribute__ ((weak)) void
 __real_prefs_store_file (struct scan_globals *globals, const gchar *key_name,
@@ -78,14 +93,14 @@ __wrap_prefs_store_file (struct scan_globals *globals, const gchar *key_name,
     __real_prefs_store_file (globals, key_name, file);
 }
 
-Ensure (Handler, credentials_error)
+Ensure (Handler, credentials_ssh_port_success)
 {
   struct scan_globals *globals = NULL;
   JsonParser *parser;
   JsonReader *cred_reader = NULL;
 
   parser = json_parser_new ();
-  json_parser_load_from_data (parser, g_json_str, strlen(g_json_str), NULL);
+  json_parser_load_from_data (parser, g_json_str, strlen (g_json_str), NULL);
   cred_reader = json_reader_new (json_parser_get_root (parser));
 
   assert_true (json_reader_is_object (cred_reader));
@@ -96,6 +111,30 @@ Ensure (Handler, credentials_error)
   assert_that (prefs_get ("auth_port_ssh"), is_equal_to_string ("22022"));
 }
 
+Ensure (Handler, credentials_ssh_up)
+{
+  struct scan_globals *globals = NULL;
+  JsonParser *parser;
+  JsonReader *cred_reader = NULL;
+
+  parser = json_parser_new ();
+  json_parser_load_from_data (parser, g_json_str, strlen (g_json_str), NULL);
+  cred_reader = json_reader_new (json_parser_get_root (parser));
+
+  assert_true (json_reader_is_object (cred_reader));
+  store_file_use_real = 0;
+  store_prefs_use_real = 0;
+  nvticache_initialized_real = false;
+
+  write_json_credentials_to_preferences (globals, cred_reader);
+  assert_that (prefs_get ("1.3.6.1.4.1.25623.1.0.103591:1:entry:SSH login:"),
+               is_equal_to_string ("some_username"));
+
+  assert_that (get_plugin_preference ("1.3.6.1.4.1.25623.1.0.103591", NULL, 1),
+               is_equal_to_string ("some_username"));
+  assert_that (get_plugin_preference ("1.3.6.1.4.1.25623.1.0.103591", NULL, 3),
+               is_equal_to_string ("super_secret"));
+}
 
 static const gchar *g_json_str_port_error =
   "{\"ssh\": {\r\n   \"username\": \"some_username\",\r\n   \"password\": "
@@ -109,7 +148,8 @@ Ensure (Handler, ssh_credentials_port_error)
   JsonReader *cred_reader = NULL;
 
   parser = json_parser_new ();
-  json_parser_load_from_data (parser, g_json_str_port_error, strlen(g_json_str_port_error), NULL);
+  json_parser_load_from_data (parser, g_json_str_port_error,
+                              strlen (g_json_str_port_error), NULL);
   cred_reader = json_reader_new (json_parser_get_root (parser));
 
   assert_true (json_reader_is_object (cred_reader));
@@ -124,7 +164,8 @@ TestSuite *
 handler_preferences_tests ()
 {
   TestSuite *suite = create_test_suite ();
-  add_test_with_context (suite, Handler, credentials_error);
+  add_test_with_context (suite, Handler, credentials_ssh_port_success);
+  add_test_with_context (suite, Handler, credentials_ssh_up);
   add_test_with_context (suite, Handler, ssh_credentials_port_error);
 
   return suite;
