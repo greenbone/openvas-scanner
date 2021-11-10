0a18da8653a5a0d30b504569d00f596c6c52ff39
Juan Jose Nicola <juan.nicola@greenbone.net>
2021-11-10 04:25:43 -0600

    Add nasl function nasl_send_arp_request().
    The new function sends an ARP request and return the target's mac address

---
+++ nasl/nasl_frame_forgery.h
@@ -0,0 +1,33 @@
+/* Copyright (C) 2021 Greenbone Networks GmbH
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+/**
+ * @file nasl_frame_forgery.h
+ * @brief Header file for module nasl_frame_forgery.
+ */
+
+#ifndef NASL_FRAME_FORGERY_H
+#define NASL_FRAME_FORGERY_H
+
+#include "nasl_lex_ctxt.h"
+
+tree_cell *
+nasl_send_arp_request (lex_ctxt *);
+
+#endif // NASL_FRAME_FORGERY_H
