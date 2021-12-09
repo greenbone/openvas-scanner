1549c91965ff9b9f139cd3313ce602325e69602f
Juan Jose Nicola <juan.nicola@greenbone.net>
2021-12-09 02:23:57 -0600

    Check 'reader' for NULL before trying to unreference it during cleanup

--- misc/table_driven_lsc.c
+++ misc/table_driven_lsc.c
@@ -161,7 +161,7 @@ get_status_of_table_driven_lsc_from_json (const char *scan_id,
                                           int len)
 {
   JsonParser *parser;
-  JsonReader *reader;
+  JsonReader *reader = NULL;
 
   GError *err = NULL;
   gchar *ret = NULL;
@@ -206,7 +206,8 @@ get_status_of_table_driven_lsc_from_json (const char *scan_id,
   json_reader_end_member (reader);
 
 cleanup:
-  g_object_unref (reader);
+  if (reader)
+    g_object_unref (reader);
   g_object_unref (parser);
   if (err != NULL)
     {
