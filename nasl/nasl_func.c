db9e424d29a1f08c67e7af58ee33274b013ac028
Juan Jose Nicola <juan.nicola@greenbone.net>
2022-10-13 12:41:56 +0200

    Fix: memory leak. Free trace buffer on error
    (cherry picked from commit 6033cc54f08964d5da00f412fe22165ce84b5438)

--- nasl/nasl_func.c
+++ nasl/nasl_func.c
@@ -239,7 +239,7 @@ nasl_func_call (lex_ctxt *lexic, const nasl_func *f, tree_cell *arg_list)
   return retc;
 
 error:
-  g_free (trace_buf)
+  g_free (trace_buf);
   free_lex_ctxt (lexic2);
   return NULL;
 }
