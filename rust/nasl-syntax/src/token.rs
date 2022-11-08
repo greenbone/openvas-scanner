95482909b1ea7461fc02c38e9412047d4b101782
Philipp Eder <philipp.eder@greenbone.net>
2022-11-08 13:31:06 +0100

    Remove Hash category
    Since
    ```
    \# this is a comment
    ```
    will be translated to a comment category there is no need for a Hash
    one.

--- rust/nasl-syntax/src/token.rs
+++ rust/nasl-syntax/src/token.rs
@@ -104,7 +104,6 @@ pub enum Category {
     LeftCurlyBracket,           // {
     RightCurlyBracket,          // }
     Comma,                      // ,
-    Hash,                       // #
     Dot,                        // .
     Percent,                    // %
     Semicolon,                  // ;
