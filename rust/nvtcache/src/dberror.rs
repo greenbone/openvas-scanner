ad0bc71995f9aeedac98c298c10b347610812e62
Ing. Juan José Nicola <jjnicola@gmail.com>
2022-11-11 12:21:04 +0100

    Adjust modules

--- rust/nvtcache/src/dberror.rs
+++ rust/nvtcache/src/dberror.rs
@@ -2,38 +2,34 @@ use redis::*;
 use std::error;
 use std::fmt;
 
-pub mod dberror {
-    use super::*;
+pub type Result<T> = std::result::Result<T, DbError>;
 
-    pub type Result<T> = std::result::Result<T, DbError>;
+#[derive(Debug)]
+pub enum DbError {
+    RedisErr(RedisError),
+    CustomErr(String),
+}
 
-    #[derive(Debug)]
-    pub enum DbError {
-        RedisErr(RedisError),
-        CustomErr(String),
-    }
-
-    impl fmt::Display for DbError {
-        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-            match &*self {
-                DbError::RedisErr(..) => write!(f, "Redis Error"),
-                DbError::CustomErr(e) => write!(f, "Error: {}", e),
-            }
+impl fmt::Display for DbError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match &*self {
+            DbError::RedisErr(..) => write!(f, "Redis Error"),
+            DbError::CustomErr(e) => write!(f, "Error: {}", e),
         }
     }
+}
 
-    impl error::Error for DbError {
-        fn source(&self) -> Option<&(dyn error::Error + 'static)> {
-            match *self {
-                DbError::RedisErr(ref e) => Some(e),
-                DbError::CustomErr(_) => None,
-            }
+impl error::Error for DbError {
+    fn source(&self) -> Option<&(dyn error::Error + 'static)> {
+        match *self {
+            DbError::RedisErr(ref e) => Some(e),
+            DbError::CustomErr(_) => None,
         }
     }
+}
 
-    impl From<RedisError> for DbError {
-        fn from(err: RedisError) -> DbError {
-            DbError::RedisErr(err)
-        }
+impl From<RedisError> for DbError {
+    fn from(err: RedisError) -> DbError {
+        DbError::RedisErr(err)
     }
 }
--- rust/nvtcache/src/redisconnector.rs
+++ rust/nvtcache/src/redisconnector.rs
@@ -3,192 +3,188 @@ use redis::*;
 const GLOBAL_DBINDEX_NAME: &str = "GVM.__GlobalDBIndex";
 const REDIS_DEFAULT_PATH: &str = "unix:///run/redis/redis-server.sock";
 
-pub mod redisconnector {
+use crate::dberror::DbError;
+use crate::dberror::Result;
 
-    use super::*;
-    use crate::dberror::dberror::DbError;
-    use crate::dberror::dberror::Result;
+pub enum KbNvtPos {
+    NvtFilenamePos,
+    NvtRequiredKeysPos,
+    NvtMandatoryKeysPos,
+    NvtExcludedKeysPos,
+    NvtRequiredUDPPortsPos,
+    NvtRequiredPortsPos,
+    NvtDependenciesPos,
+    NvtTagsPos,
+    NvtCvesPos,
+    NvtBidsPos,
+    NvtXrefsPos,
+    NvtCategoryPos,
+    NvtFamilyPos,
+    NvtNamePos,
+    NvtTimestampPos,
+    NvtOIDPos,
+}
 
-    pub enum KbNvtPos {
-        NvtFilenamePos,
-        NvtRequiredKeysPos,
-        NvtMandatoryKeysPos,
-        NvtExcludedKeysPos,
-        NvtRequiredUDPPortsPos,
-        NvtRequiredPortsPos,
-        NvtDependenciesPos,
-        NvtTagsPos,
-        NvtCvesPos,
-        NvtBidsPos,
-        NvtXrefsPos,
-        NvtCategoryPos,
-        NvtFamilyPos,
-        NvtNamePos,
-        NvtTimestampPos,
-        NvtOIDPos,
-    }
+pub struct RedisCtx {
+    kb: Connection, //a redis connection
+    db: u32,        // the name space
+    maxdb: u32,     // max db index
+}
 
-    pub struct RedisCtx {
-        kb: Connection, //a redis connection
-        db: u32,        // the name space
-        maxdb: u32,     // max db index
-    }
+#[derive(Debug, PartialEq)]
+pub struct RedisValueHandler {
+    v: String,
+}
 
-    #[derive(Debug, PartialEq)]
-    pub struct RedisValueHandler {
-        v: String,
-    }
-
-    impl FromRedisValue for RedisValueHandler {
-        fn from_redis_value(v: &Value) -> RedisResult<RedisValueHandler> {
-            match v {
-                Value::Nil => Ok(RedisValueHandler { v: String::new() }),
-                _ => {
-                    let new_var: String = from_redis_value(v).unwrap_or("".to_string());
-                    Ok(RedisValueHandler { v: new_var })
-                }
+impl FromRedisValue for RedisValueHandler {
+    fn from_redis_value(v: &Value) -> RedisResult<RedisValueHandler> {
+        match v {
+            Value::Nil => Ok(RedisValueHandler { v: String::new() }),
+            _ => {
+                let new_var: String = from_redis_value(v).unwrap_or("".to_string());
+                Ok(RedisValueHandler { v: new_var })
             }
         }
     }
+}
 
-    impl RedisCtx {
-        /// Connect to the redis server and return a redis context object
-        pub fn new() -> Result<RedisCtx> {
-            let client = redis::Client::open(REDIS_DEFAULT_PATH)?;
-            let kb = client.get_connection()?;
-            let mut redisctx = RedisCtx {
-                kb,
-                db: 0,
-                maxdb: 0,
-            };
-            let _kbi = redisctx.select_database()?;
-            Ok(redisctx)
+impl RedisCtx {
+    /// Connect to the redis server and return a redis context object
+    pub fn new() -> Result<RedisCtx> {
+        let client = redis::Client::open(REDIS_DEFAULT_PATH)?;
+        let kb = client.get_connection()?;
+        let mut redisctx = RedisCtx {
+            kb,
+            db: 0,
+            maxdb: 0,
+        };
+        let _kbi = redisctx.select_database()?;
+        Ok(redisctx)
+    }
+
+    /// Get the max db index configured for the redis server instance
+    fn max_db_index(&mut self) -> Result<u32> {
+        if self.maxdb > 0 {
+            return Ok(self.maxdb);
         }
 
-        /// Get the max db index configured for the redis server instance
-        fn max_db_index(&mut self) -> Result<u32> {
-            if self.maxdb > 0 {
+        let maxdb = Cmd::new()
+            .arg("CONFIG")
+            .arg("GET")
+            .arg("databases")
+            .query(&mut self.kb);
+
+        match maxdb {
+            Ok(mdb) => {
+                let res: Vec<String> = mdb;
+                self.maxdb = max_db_index_to_uint(res);
                 return Ok(self.maxdb);
             }
-
-            let maxdb = Cmd::new()
-                .arg("CONFIG")
-                .arg("GET")
-                .arg("databases")
-                .query(&mut self.kb);
-
-            match maxdb {
-                Ok(mdb) => {
-                    let res: Vec<String> = mdb;
-                    self.maxdb = max_db_index_to_uint(res);
-                    return Ok(self.maxdb);
-                }
-                Err(_) => {
-                    return Err(DbError::CustomErr(String::from(
-                        "Not possible to select a free database.",
-                    )))
-                }
+            Err(_) => {
+                return Err(DbError::CustomErr(String::from(
+                    "Not possible to select a free database.",
+                )))
             }
-            /// Redis always replies about config with a vector
-            /// of 2 string ["databases", "Number"]
-            /// Therefore we convert the "Number" to uint32
-            fn max_db_index_to_uint(res: Vec<String>) -> u32 {
-                if res.len() == 2 {
-                    match res[1].to_string().parse::<u32>() {
-                        Ok(m) => return m,
-                        Err(e) => {
-                            println!("{}", e);
-                            return 0 as u32;
-                        }
+        }
+        /// Redis always replies about config with a vector
+        /// of 2 string ["databases", "Number"]
+        /// Therefore we convert the "Number" to uint32
+        fn max_db_index_to_uint(res: Vec<String>) -> u32 {
+            if res.len() == 2 {
+                match res[1].to_string().parse::<u32>() {
+                    Ok(m) => return m,
+                    Err(e) => {
+                        println!("{}", e);
+                        return 0 as u32;
                     }
                 }
-                return 0 as u32;
+            }
+            return 0 as u32;
+        }
+    }
+
+    pub fn get_namespace(&mut self) -> Result<u32> {
+        let db: u32 = self.db;
+        Ok(db)
+    }
+
+    fn set_namespace(&mut self, db_index: u32) -> Result<String> {
+        Cmd::new()
+            .arg("SELECT")
+            .arg(db_index.to_string())
+            .query(&mut self.kb)?;
+
+        self.db = db_index;
+        return Ok(String::from("ok"));
+    }
+
+    fn try_database(&mut self, dbi: u32) -> Result<u32> {
+        let ret = self.kb.hset_nx(GLOBAL_DBINDEX_NAME, dbi, 1)?;
+        Ok(ret)
+    }
+
+    fn select_database(&mut self) -> Result<u32> {
+        let maxdb: u32 = self.max_db_index()?;
+        let mut selected_db: u32 = 0;
+
+        // Start always from 1. Namespace 0 is reserved
+        //format GLOBAL_DBINDEX_NAME
+        for i in 1..maxdb {
+            let ret = self.try_database(i)?;
+            if ret == 1 {
+                selected_db = i;
+                break;
             }
         }
-
-        pub fn get_namespace(&mut self) -> Result<u32> {
-            let db: u32 = self.db;
-            Ok(db)
+        if selected_db > 0 {
+            self.set_namespace(selected_db)?;
+            return Ok(self.db);
         }
+        return Err(DbError::CustomErr(String::from(
+            "Not possible to select a free db",
+        )));
+    }
 
-        fn set_namespace(&mut self, db_index: u32) -> Result<String> {
-            Cmd::new()
-                .arg("SELECT")
-                .arg(db_index.to_string())
-                .query(&mut self.kb)?;
+    /// Delete an entry from the in-use namespace's list
+    fn release_namespace(&mut self) -> Result<()> {
+        // Get firstthe current db index, the one to be released
+        let dbi = self.get_namespace()?;
+        // Remove the entry from the hash list
+        self.set_namespace(0)?;
+        self.kb.hdel(GLOBAL_DBINDEX_NAME, dbi)?;
+        Ok(())
+    }
 
-            self.db = db_index;
-            return Ok(String::from("ok"));
-        }
+    /// Delete all keys in the namespace and relase the it
+    pub fn delete_namespace(&mut self) -> Result<()> {
+        Cmd::new().arg("FLUSHDB").query(&mut self.kb)?;
+        self.release_namespace()?;
+        Ok(())
+    }
+    //Wrapper function to avoid accessing kb member directly.
+    pub fn redis_set_key<T: ToRedisArgs>(&mut self, key: &str, val: T) -> Result<()> {
+        let _: () = self.kb.set(key, val)?;
+        Ok(())
+    }
 
-        fn try_database(&mut self, dbi: u32) -> Result<u32> {
-            let ret = self.kb.hset_nx(GLOBAL_DBINDEX_NAME, dbi, 1)?;
-            Ok(ret)
-        }
+    pub fn redis_add_item<T: ToRedisArgs>(&mut self, key: String, val: T) -> Result<String> {
+        let ret: RedisValueHandler = self.kb.lpush(key, val)?;
+        Ok(ret.v)
+    }
 
-        fn select_database(&mut self) -> Result<u32> {
-            let maxdb: u32 = self.max_db_index()?;
-            let mut selected_db: u32 = 0;
+    pub fn redis_get_key(&mut self, key: &str) -> Result<String> {
+        let ret: RedisValueHandler = self.kb.get(key)?;
+        Ok(ret.v)
+    }
 
-            // Start always from 1. Namespace 0 is reserved
-            //format GLOBAL_DBINDEX_NAME
-            for i in 1..maxdb {
-                let ret = self.try_database(i)?;
-                if ret == 1 {
-                    selected_db = i;
-                    break;
-                }
-            }
-            if selected_db > 0 {
-                self.set_namespace(selected_db)?;
-                return Ok(self.db);
-            }
-            return Err(DbError::CustomErr(String::from(
-                "Not possible to select a free db",
-            )));
-        }
+    pub fn redis_get_item(&mut self, key: String, index: KbNvtPos) -> Result<String> {
+        let ret: RedisValueHandler = self.kb.lindex(key, index as isize)?;
 
-        /// Delete an entry from the in-use namespace's list
-        fn release_namespace(&mut self) -> Result<()> {
-            // Get firstthe current db index, the one to be released
-            let dbi = self.get_namespace()?;
-            // Remove the entry from the hash list
-            self.set_namespace(0)?;
-            self.kb.hdel(GLOBAL_DBINDEX_NAME, dbi)?;
-            Ok(())
-        }
+        Ok(ret.v)
+    }
 
-        /// Delete all keys in the namespace and relase the it
-        pub fn delete_namespace(&mut self) -> Result<()> {
-            Cmd::new().arg("FLUSHDB").query(&mut self.kb)?;
-            self.release_namespace()?;
-            Ok(())
-        }
-        //Wrapper function to avoid accessing kb member directly.
-        pub fn redis_set_key<T: ToRedisArgs>(&mut self, key: &str, val: T) -> Result<()> {
-            let _: () = self.kb.set(key, val)?;
-            Ok(())
-        }
-
-        pub fn redis_add_item<T: ToRedisArgs>(&mut self, key: String, val: T) -> Result<String> {
-            let ret: RedisValueHandler = self.kb.lpush(key, val)?;
-            Ok(ret.v)
-        }
-
-        pub fn redis_get_key(&mut self, key: &str) -> Result<String> {
-            let ret: RedisValueHandler = self.kb.get(key)?;
-            Ok(ret.v)
-        }
-
-        pub fn redis_get_item(&mut self, key: String, index: KbNvtPos) -> Result<String> {
-            let ret: RedisValueHandler = self.kb.lindex(key, index as isize)?;
-
-            Ok(ret.v)
-        }
-
-        pub fn redis_del_key(&mut self, key: String) -> Result<String> {
-            let ret: RedisValueHandler = self.kb.del(key)?;
-            Ok(ret.v)
-        }
+    pub fn redis_del_key(&mut self, key: String) -> Result<String> {
+        let ret: RedisValueHandler = self.kb.del(key)?;
+        Ok(ret.v)
     }
 }
