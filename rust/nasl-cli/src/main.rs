7d26f2d66781c8838e42d80335bd425af0e27280
Philipp Eder <philipp.eder@greenbone.net>
2022-11-18 15:47:46 +0100

    Refactor cleanup nasl-cli a bit
    Created a function print_result to have the same behaviour on a single
    file as on a directory

--- rust/nasl-cli/src/main.rs
+++ rust/nasl-cli/src/main.rs
@@ -60,6 +60,29 @@ fn read<P: AsRef<Path>>(path: P) -> Result<Vec<Result<Statement, SyntaxError>>, 
     Ok(nasl_syntax::parse(&code).collect())
 }
 
+fn print_results(path: &Path, verbose: bool) -> usize {
+    let mut errors = 0;
+
+    if verbose {
+        println!("# {:?}", path);
+        let results = read(path).unwrap();
+        for r in results {
+            match r {
+                Ok(stmt) => println!("{:?}", stmt),
+                Err(err) => eprintln!("{}", err),
+            }
+        }
+    } else {
+        let err = read_errors(&path).unwrap();
+        if !err.is_empty() {
+            eprintln!("# Error in {:?}", path);
+        }
+        errors += err.len();
+        err.iter().for_each(|r| eprintln!("{}", r));
+    }
+    errors
+}
+
 fn syntax_check(path: PathBuf, verbose: bool) {
     let mut parsed: usize = 0;
     let mut skipped: usize = 0;
@@ -67,7 +90,7 @@ fn syntax_check(path: PathBuf, verbose: bool) {
     println!("verifiying NASL syntax in {:?}.", path);
     if path.as_path().is_dir() {
         for entry in WalkDir::new(path).into_iter().filter_map(|e| e.ok()) {
-                print!("\rparsing {}th file", parsed);
+            print!("\rparsing {}th file", parsed);
             let ext = {
                 if let Some(ext) = entry.path().extension() {
                     ext.to_str().unwrap().to_owned()
@@ -78,36 +101,14 @@ fn syntax_check(path: PathBuf, verbose: bool) {
             if !matches!(ext.as_str(), "nasl" | "inc") {
                 skipped += 1;
             } else {
-                if verbose {
-                    println!("# {:?}", entry.path());
-                    let results = read(entry.path()).unwrap();
-                    for r in results {
-                        match r {
-                            Ok(stmt) => println!("{:?}", stmt),
-                            Err(err) => eprintln!("{}", err),
-                        }
-                    }
-                } else {
-                    let err = read_errors(entry.path()).unwrap();
-                    if !err.is_empty() {
-                        eprintln!("# Error in {:?}", entry.path());
-                    }
-                    errors += err.len();
-                    err.iter().for_each(|r| eprintln!("{}", r));
-                }
+                errors += print_results(entry.path(), verbose);
                 parsed += 1;
             }
         }
         println!();
     } else {
-        println!("# {:?}", path);
-        let results = read(path).unwrap();
-        for r in results {
-            match r {
-                Ok(stmt) => println!("{:?}", stmt),
-                Err(err) => eprintln!("{}", err),
-            }
-        }
+        errors += print_results(path.as_path(), verbose);
+        parsed += 1;
     }
     println!(
         "skipped: {} files; parsed: {} files; errors: {}",
@@ -120,6 +121,4 @@ fn main() {
     match cli.command {
         Command::Syntax { path, verbose } => syntax_check(path, verbose),
     }
-
-    //       let code: String = fs::read(path).map(|bs| bs.iter().map(|&b| b as char).collect())?;
 }
