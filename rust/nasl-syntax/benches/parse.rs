a22ab3a93d15995a0fbb882777c29f706af4c450
Philipp Eder <philipp.eder@greenbone.net>
2022-11-23 14:12:49 +0100

    Refactor: add rudimentary benchmark function with criterion
    To have a insight if a change in nasl-syntax impacts the performance a
    small benchmark that parses an nasl file was added.
    
    To execute benchmarks run:
    ```
    cargo bench
    ```
    
    if you want to have a html output you need to install
    
    - cargo-criterion (`cargo install cargo-criterion `)
    - gnuplot
    
    and then run it via:
    
    ```
    cargo criterion
    ```

---
+++ rust/nasl-syntax/benches/parse.rs
@@ -0,0 +1,28 @@
+use std::{env, fs, path::PathBuf};
+
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use nasl_syntax::parse;
+
+fn loadfile(filename: &str) -> PathBuf {
+    let mut current = env::current_dir().unwrap();
+    let reset = current.clone();
+    // move to nasl-syntax
+    current.push("nasl-syntax");
+    if !current.is_dir() {
+        // we were already in nasl-syntax and have to reset
+        current = reset;
+    }
+    current.push("benches");
+    current.push(filename);
+    current
+}
+
+pub fn simple_parse_benchmark(c: &mut Criterion) {
+    let code: String = fs::read(loadfile("simple_parse.nasl"))
+        .map(|bs| bs.iter().map(|&b| b as char).collect())
+        .unwrap();
+    c.bench_function("simple_parse", |b| b.iter(|| parse(black_box(&code))));
+}
+
+criterion_group!(benches, simple_parse_benchmark);
+criterion_main!(benches);
