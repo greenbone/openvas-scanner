ab22a3ffbc2b0043b520cbf2aa330d4cfc27f91c
Philipp Eder <philipp.eder@greenbone.net>
2023-01-23 10:49:47 +0100

    Implement raw_string
    raw_string is a NASL function to parse any number to ascii
    representation and combine them with all additional parameters.
    
    As an example:
    
    ```
    raw_string(0x7B, "Hallo");
    ```
    
    returns "{Hallo".

--- rust/nasl-interpreter/src/built_in_functions/string.rs
+++ rust/nasl-interpreter/src/built_in_functions/string.rs
@@ -1,13 +1,13 @@
 //! Defines NASL functions that deal with string and their helpers
 
-use core::fmt::Write;
+use core::fmt::{self, Write};
 use sink::Sink;
 
 use crate::{
     error::FunctionError, lookup_keys::FC_ANON_ARGS, ContextType, NaslFunction, NaslValue, Register,
 };
 
-pub fn resolve_positional_arguments(register: &Register) -> Vec<NaslValue> {
+fn resolve_positional_arguments(register: &Register) -> Vec<NaslValue> {
     match register.named(FC_ANON_ARGS).cloned() {
         // TODO maybe we need to resolve those nasl values?
         Some(ContextType::Value(NaslValue::Array(arr))) => arr,
@@ -15,25 +15,62 @@ pub fn resolve_positional_arguments(register: &Register) -> Vec<NaslValue> {
     }
 }
 
+impl From<fmt::Error> for FunctionError {
+    fn from(e: fmt::Error) -> Self {
+        Self {
+            reason: format!("{}", e),
+        }
+    }
+}
+
+fn write_nasl_value(s: &mut String, value: &NaslValue) -> Result<(), FunctionError> {
+    match value {
+        NaslValue::String(x) => write!(s, "{}", x),
+        NaslValue::Number(x) => {
+            let c = *x as u8 as char;
+            if c.is_ascii_graphic() {
+                write!(s, "{}", c)
+            } else {
+                write!(s, ".")
+            }
+        }
+        NaslValue::Array(x) => {
+            for p in x {
+                write_nasl_value(s, p)?;
+            }
+            Ok(())
+        }
+        NaslValue::Dict(x) => {
+            for p in x.values() {
+                write_nasl_value(s, p)?;
+            }
+            Ok(())
+        }
+        _ => write!(s, "."),
+    }?;
+    Ok(())
+}
+
+fn raw_string(_: &str, _: &dyn Sink, register: &Register) -> Result<NaslValue, FunctionError> {
+    let positional = resolve_positional_arguments(register);
+    let mut s = String::with_capacity(2 * positional.len());
+    for p in positional {
+        write_nasl_value(&mut s, &p)?;
+    }
+    Ok(NaslValue::String(s))
+}
 /// NASL function to return  a hex presentation of given string as a positional argument.
 ///
 /// If the positional arguments are empty it reutnrns NaslValue::Null.
 /// It only uses the first positional argument and when it is not a NaslValue:String than it returns NaslValue::Null.
-pub fn hexstr(_: &str, _: &dyn Sink, register: &Register) -> Result<NaslValue, FunctionError> {
+fn hexstr(_: &str, _: &dyn Sink, register: &Register) -> Result<NaslValue, FunctionError> {
     let positional = resolve_positional_arguments(register);
     // we already checked that positional has at least one argument
     Ok(match positional.get(0) {
         Some(NaslValue::String(x)) => {
             let mut s = String::with_capacity(2 * x.len());
             for byte in x.as_bytes() {
-                match write!(s, "{:02X}", byte) {
-                    Ok(_) => {}
-                    Err(e) => {
-                        return Err(FunctionError {
-                            reason: format!("Unable to parse {} to hex: {}", byte, e),
-                        })
-                    }
-                };
+                write!(s, "{:02X}", byte)?;
             }
             NaslValue::String(s)
         }
@@ -45,6 +82,7 @@ pub fn hexstr(_: &str, _: &dyn Sink, register: &Register) -> Result<NaslValue, F
 pub fn lookup(key: &str) -> Option<NaslFunction> {
     match key {
         "hexstr" => Some(hexstr),
+        "raw_string" => Some(raw_string),
         _ => None,
     }
 }
@@ -55,6 +93,12 @@ mod tests {
     use sink::DefaultSink;
 
     use crate::{Interpreter, NaslValue, NoOpLoader, Register};
+
+    impl From<&str> for NaslValue {
+        fn from(s: &str) -> Self {
+            Self::String(s.to_owned())
+        }
+    }
 
     #[test]
     fn hexstr() {
@@ -72,15 +116,26 @@ mod tests {
         let mut parser =
             parse(code).map(|x| interpreter.resolve(&x.expect("no parse error expected")));
         parser.next();
-        assert_eq!(
-            parser.next(),
-            Some(Ok(NaslValue::String("666F6F".to_owned())))
-        );
-        assert_eq!(
-            parser.next(),
-            Some(Ok(NaslValue::String("666F6F".to_owned())))
-        );
+        assert_eq!(parser.next(), Some(Ok("666F6F".into())));
+        assert_eq!(parser.next(), Some(Ok("666F6F".into())));
         assert_eq!(parser.next(), Some(Ok(NaslValue::Null)));
         assert_eq!(parser.next(), Some(Ok(NaslValue::Null)));
+    }
+    #[test]
+    fn raw_string() {
+        let code = r###"
+        raw_string(0x7B);
+        raw_string(0x7B, 1);
+        raw_string(0x7B, 1, "Hallo");
+        "###;
+        let storage = DefaultSink::new(false);
+        let mut register = Register::default();
+        let loader = NoOpLoader::default();
+        let mut interpreter = Interpreter::new("1", &storage, &loader, &mut register);
+        let mut parser =
+            parse(code).map(|x| interpreter.resolve(&x.expect("no parse error expected")));
+        assert_eq!(parser.next(), Some(Ok("{".into())));
+        assert_eq!(parser.next(), Some(Ok("{.".into())));
+        assert_eq!(parser.next(), Some(Ok("{.Hallo".into())));
     }
 }
