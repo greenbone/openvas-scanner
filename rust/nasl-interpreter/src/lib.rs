c978e5b63557fd9e13fa885f7437fd1261f3dc71
Philipp Eder <philipp.eder@greenbone.net>
2023-01-23 12:28:21 +0100

    Fix typos in documentation
    Co-authored-by: Christoph Krämer <c.kraemer.rulle@web.de>

--- rust/nasl-interpreter/src/lib.rs
+++ rust/nasl-interpreter/src/lib.rs
@@ -1,4 +1,4 @@
-//! Is a crate to use Stataments from nasl-syntax and execute them.
+//! Is a crate to use Statements from nasl-syntax and execute them.
 #![warn(missing_docs)]
 use built_in_functions::description;
 mod error;
@@ -21,7 +21,7 @@ pub use interpreter::{Interpreter, NaslValue};
 pub use loader::*;
 use sink::{Sink, SinkError};
 
-// Is a type definition for inbuild functions
+// Is a type definition for built-in functions
 pub(crate) type NaslFunction = fn(&str, &dyn Sink, &Register) -> Result<NaslValue, FunctionError>;
 pub(crate) fn lookup(function_name: &str) -> Option<NaslFunction> {
     description::lookup(function_name)
