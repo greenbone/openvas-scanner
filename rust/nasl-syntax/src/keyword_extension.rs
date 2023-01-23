beastycoding-forks/greenbone.openvas-scanner

Add support for loops in the NASL Rust implementation
This PR introduces a new module which adds support to interprete loop structures in the rust implementation of NASL.
In order to support the loop control flow keywords `continue` and `break` the nasl-syntax was adjusted to parse them.
To be able to actually work with these controll flow keywords, new `NaslValue`s are introduced.

rust/nasl-syntax/src/keyword_extension.rs
@@ -166,6 +166,38 @@ impl<'a> Lexer<'a> {
             Err(unexpected_end!("exit"))
         }
     }
+    fn parse_continue(&mut self) -> Result<(End, Statement), SyntaxError> {
+        let token = self.peek();
+        if let Some(token) = token {
+            if matches!(token.category(), Category::Semicolon) {
+                self.token();
+                return Ok((
+                    End::Done(Category::Semicolon),
+                    Statement::Continue,
+                ));
+            }
+            else {
+                return Err(unexpected_token!(token));
+            }
+        }
+        Err(unexpected_end!("exit"))
+    }
+    fn parse_break(&mut self) -> Result<(End, Statement), SyntaxError> {
+        let token = self.peek();
+        if let Some(token) = token {
+            if matches!(token.category(), Category::Semicolon) {
+                self.token();
+                return Ok((
+                    End::Done(Category::Semicolon),
+                    Statement::Break,
+                ));
+            }
+            else {
+                return Err(unexpected_token!(token));
+            }
+        }
+        Err(unexpected_end!("exit"))
+    }
     fn parse_for(&mut self) -> Result<(End, Statement), SyntaxError> {
         self.jump_to_left_parenthesis()?;
         let (end, assignment) = self.statement(0, &|c| c == Category::Semicolon)?;
@@ -318,6 +350,8 @@ impl<'a> Keywords for Lexer<'a> {
             Keyword::GlobalVar => self.parse_declaration(DeclareScope::Global),
             Keyword::Null => Ok((End::Continue, Statement::Primitive(token))),
             Keyword::Return => self.parse_return(),
+            Keyword::Continue => self.parse_continue(),
+            Keyword::Break => self.parse_break(),
             Keyword::Include => self.parse_include(),
             Keyword::Exit => self.parse_exit(),
             Keyword::FCTAnonArgs => self.parse_fct_anon_args(token),

rust/nasl-syntax/src/token.rs
@@ -142,6 +142,10 @@ pub enum Keyword {
     Null,
     /// return
     Return,
+    /// continue
+    Continue,
+    /// break
+    Break,
     /// include
     Include,
     /// Scanning phases; can be set by category in the description block
@@ -173,6 +177,8 @@ make_keyword_matcher! {
     global_var: Keyword::GlobalVar,
     NULL: Keyword::Null,
     return: Keyword::Return,
+    continue: Keyword::Continue,
+    break: Keyword::Break,
     include: Keyword::Include,
     exit: Keyword::Exit,
     ACT_ATTACK: Keyword::ACT(ACT::Attack),
@@ -859,6 +865,9 @@ mod tests {
         verify_tokens!("return", vec![(Identifier(Some(Return)), 0, 6)]);
         verify_tokens!("include", vec![(Identifier(Some(Include)), 0, 7)]);
         verify_tokens!("exit", vec![(Identifier(Some(Exit)), 0, 4)]);
+        verify_tokens!("break", vec![(Identifier(Some(Break)), 0, 5)]);
+        verify_tokens!("continue", vec![(Identifier(Some(Continue)), 0, 8)]);
+        
     }
 
     #[test]

greenbone/openvas-scanner

rust/nasl-syntax/src/keyword_extension.rs
@@ -169,6 +169,30 @@ impl<'a> Lexer<'a> {
             Err(unexpected_end!("exit"))
         }
     }
+    fn parse_continue(&mut self) -> Result<(End, Statement), SyntaxError> {
+        let token = self.peek();
+        if let Some(token) = token {
+            if matches!(token.category(), Category::Semicolon) {
+                self.token();
+                return Ok((End::Done(Category::Semicolon), Statement::Continue));
+            } else {
+                return Err(unexpected_token!(token));
+            }
+        }
+        Err(unexpected_end!("exit"))
+    }
+    fn parse_break(&mut self) -> Result<(End, Statement), SyntaxError> {
+        let token = self.peek();
+        if let Some(token) = token {
+            if matches!(token.category(), Category::Semicolon) {
+                self.token();
+                return Ok((End::Done(Category::Semicolon), Statement::Break));
+            } else {
+                return Err(unexpected_token!(token));
+            }
+        }
+        Err(unexpected_end!("exit"))
+    }
     fn parse_for(&mut self) -> Result<(End, Statement), SyntaxError> {
         self.jump_to_left_parenthesis()?;
         let (end, assignment) = self.statement(0, &|c| c == &Category::Semicolon)?;
@@ -326,6 +350,8 @@ impl<'a> Keywords for Lexer<'a> {
                 Ok((End::Continue, Statement::AttackCategory(category)))
             }
             IdentifierType::Undefined(_) => Err(unexpected_token!(token)),
+            IdentifierType::Continue => self.parse_continue(),
+            IdentifierType::Break => self.parse_break(),
         }
     }
 }

rust/nasl-syntax/src/token.rs
@@ -145,6 +145,10 @@ pub enum IdentifierType {
     Null,
     /// return
     Return,
+    /// continue
+    Continue,
+    /// break
+    Break,
     /// include
     Include,
     /// Scanning phases; can be set by category in the description block
@@ -183,7 +187,9 @@ make_keyword_matcher! {
     ACT_KILL_HOST: IdentifierType::ACT(ACT::KillHost),
     ACT_MIXED_ATTACK: IdentifierType::ACT(ACT::MixedAttack),
     ACT_SCANNER: IdentifierType::ACT(ACT::Scanner),
-    ACT_SETTINGS: IdentifierType::ACT(ACT::Settings)
+    ACT_SETTINGS: IdentifierType::ACT(ACT::Settings),
+    continue: IdentifierType::Continue,
+    break: IdentifierType::Break
 }
 
 /// Is used to identify a Token
@@ -582,7 +588,7 @@ impl<'a> Tokenizer<'a> {
 
     // checks if a number is binary, octal, base10 or hex
     #[inline(always)]
-    pub fn tokenize_number(&mut self, mut start: usize, current: char) -> Category {
+    fn tokenize_number(&mut self, mut start: usize, current: char) -> Category {
         use Base::*;
         let may_base = {
             if current == '0' {
@@ -945,6 +951,8 @@ mod tests {
         verify_tokens!("return", vec![(Identifier(Return), 1, 1)]);
         verify_tokens!("include", vec![(Identifier(Include), 1, 1)]);
         verify_tokens!("exit", vec![(Identifier(Exit), 1, 1)]);
+        verify_tokens!("break", vec![(Identifier(Break), 1, 1)]);
+        verify_tokens!("continue", vec![(Identifier(Continue), 1, 1)]);
     }
 
     #[test]