77d951e0a5b4e80bd561fd902e1917834cb6f4a0
Philipp Eder <philipp.eder@greenbone.net>
2023-04-04 12:31:30 +0200

    Refactor: accept any Logable in NaslLogger trait
    Instead of forcing the caller of a logger to transform it to string they
    can directly log it when it has the `Send + Sync + Display` trait
    implemented.

--- rust/nasl-interpreter/src/logger.rs
+++ rust/nasl-interpreter/src/logger.rs
@@ -2,6 +2,8 @@
 //
 // SPDX-License-Identifier: GPL-2.0-or-later
 
+use std::fmt::Display;
+
 /// Modes that are used by the default logger
 #[derive(Eq, PartialEq, PartialOrd, Default)]
 pub enum Mode {
@@ -17,18 +19,27 @@ pub enum Mode {
     /// Disabled, no Messages are logged
     Nothing,
 }
+
+/// A trait for types that can be logged.
+///
+/// This trait is automatically implemented for all types that implement
+/// `Sync + Send + Display`.
+pub trait Logable: Sync + Send + Display {}
+
+impl<T: Sync + Send + Display> Logable for T {}
+
 /// A interface for a logger for the NASL interpreter
 pub trait NaslLogger {
     /// Print a Debug Message
-    fn debug(&self, msg: String);
+    fn debug(&self, msg: &dyn Logable);
     /// Print a Info Message
-    fn info(&self, msg: String);
+    fn info(&self, msg: &dyn Logable);
     /// Print a Warning Message
-    fn warning(&self, msg: &str);
+    fn warning(&self, msg: &dyn Logable);
     /// Print a Error Message
-    fn error(&self, msg: String);
+    fn error(&self, msg: &dyn Logable);
     /// Print a normal Message
-    fn print(&self, msg: String);
+    fn print(&self, msg: &dyn Logable);
 }
 
 /// The default logger for NASL. It will just print to the terminal. It has a
@@ -53,35 +64,35 @@ impl DefaultLogger {
 }
 
 impl NaslLogger for DefaultLogger {
-    fn debug(&self, msg: String) {
+    fn debug(&self, msg: &dyn Logable) {
         if self.mode > Mode::Debug {
             return;
         }
         println!("\x1b[38;5;8mDEBUG: \x1b[0m{}", msg);
     }
 
-    fn info(&self, msg: String) {
+    fn info(&self, msg: &dyn Logable) {
         if self.mode > Mode::Info {
             return;
         }
         println!("\x1b[38;5;2mINFO : \x1b[0m{}", msg);
     }
 
-    fn warning(&self, msg: &str) {
+    fn warning(&self, msg: &dyn Logable) {
         if self.mode > Mode::Warning {
             return;
         }
         println!("\x1b[38;5;3mWARN : \x1b[0m{}", msg);
     }
 
-    fn error(&self, msg: String) {
+    fn error(&self, msg: &dyn Logable) {
         if self.mode > Mode::Error {
             return;
         }
         println!("\x1b[38;5;1mERROR: \x1b[0m{}", msg);
     }
 
-    fn print(&self, msg: String) {
+    fn print(&self, msg: &dyn Logable) {
         println!("{}", msg);
     }
 }
