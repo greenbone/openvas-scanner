c128d85149b4d6a1d00f7f9b107fd1a00d5a73d5
Juan Jose Nicola <juan.nicola@greenbone.net>
2023-09-07 11:24:34 +0200

    Return error code if the smoke test fails. So, github workflow fails if a test fails.

--- rust/smoketest/src/main.rs
+++ rust/smoketest/src/main.rs
@@ -1,13 +1,15 @@
 mod config;
 mod tests;
 
+use std::process::ExitCode;
+
 use crate::tests::*;
 use clap::Parser;
 use config::Args;
 use smoketest::new_client;
 
 #[tokio::main]
-async fn main() {
+async fn main() -> ExitCode{
     let args = Args::parse();
     let filter = tracing_subscriber::EnvFilter::builder()
         .with_default_directive(tracing::metadata::LevelFilter::INFO.into())
@@ -19,10 +21,12 @@ async fn main() {
     tracing::info!("\n\nTEST 1: Check server connection:");
     if !test1::check_server(&cli, args.openvasd()).await {
         tracing::info!("TEST 1: FAILED:");
-        return;
+        return ExitCode::FAILURE;
     }
     tracing::info!("\n\nTEST 2: Running successful scan:");
     if !test2::run_scan(&cli, args.openvasd(), args.scan_config()).await {
         tracing::info!("TEST 2: FAILED");
+        return ExitCode::FAILURE;
     }
+    ExitCode::SUCCESS
 }
