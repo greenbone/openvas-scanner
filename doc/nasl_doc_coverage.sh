b881c7680331a6ed94a1c8d35c71e8f278eeea15
Kraemii <c.kraemer.rulle@web.de>
2023-09-19 13:11:04 +0200

    Add: script for calculate and display coverage of the rust implementation of nasl builtin-functions

--- doc/nasl_doc_coverage.sh
+++ doc/nasl_doc_coverage.sh
@@ -3,38 +3,6 @@
 #
 # SPDX-License-Identifier: GPL-2.0-or-later
 
-make_toc_entry() {
-    name=${file##*/}
-    name=${name//.md/}
-    entry=$(grep "\*\*$name\*\* - " $file)
-    entry=${entry//${name}/[${name}](${name}.md)}
-    toc="$toc\n$entry"
-}
-
-make_tocs() {
-    for dir in "$root_dir_prefix"/*
-    do
-        if [[ -d $dir ]]; then
-            toc=""
-            for file in "$dir"/*
-            do
-                if [ ${file##*/} != "index.md" ]; then
-                    make_toc_entry
-                fi
-            done
-            file_content=""
-            while read -r line
-            do
-                file_content="$file_content$line\n"
-                if [[ $line =~ "## TABLE OF CONTENT" ]]; then
-                    break
-                fi
-            done < $dir/index.md
-            printf "$file_content$toc" > $dir/index.md
-        fi
-    done
-}
-
 base_dir=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 nasl_init="$base_dir"/../nasl/nasl_init.c
 nasl_built_in="$base_dir"/manual/nasl/built-in-functions
