9f20c7530ea83761c5327657e4cd6351af44de1c
Kraemii <c.kraemer.rulle@web.de>
2023-09-19 07:38:57 +0200

    Adjust nasl-c-lib documentation

--- rust/nasl-c-lib/build.rs
+++ rust/nasl-c-lib/build.rs
@@ -7,8 +7,8 @@ fn main() {
     );
     println!("cargo:rustc-link-lib=static=gcrypt");
     println!("cargo:rustc-link-lib=static=gpg-error");
-    println!("cargo:rerun-if-changed=c/cryptographic/gcrypt_mac.h");
-    println!("cargo:rerun-if-changed=c/cryptographic/gcrypt_error.h");
+    println!("cargo:rerun-if-changed=c/cryptographic/gcrypt_mac.c");
+    println!("cargo:rerun-if-changed=c/cryptographic/gcrypt_error.c");
     println!("cargo:rerun-if-changed=lib/libgcrypt.a");
     println!("cargo:rerun-if-changed=lib/libgpg-error.a");
 
