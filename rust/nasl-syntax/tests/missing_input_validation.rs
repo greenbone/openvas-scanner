0fa13e9b8c2a04f9c4633237f45ac63a0f12606e
Philipp Eder <philipp.eder@greenbone.net>
2023-11-30 07:55:24 +0100

    Fix: nasl-cli/openvasd: stack overflow on prefix-statement (#1530)
    Reported by @sepehrdaddev.
    
    To prevent prefix cases from hitting a stack overflow due to recursion
    it is reset after grouping parsing rather than handled per statement like
    in the loop.

--- rust/nasl-syntax/tests/missing_input_validation.rs
+++ rust/nasl-syntax/tests/missing_input_validation.rs
@@ -5,16 +5,27 @@
 #[cfg(test)]
 mod test {
 
-    use nasl_syntax::{logger::NaslLogger, parse};
-
     #[test]
     fn validate_recursion_depth_to_prevent_stackoverflow() {
-        // Reported by Anon, VSS:4.0/AV:L/AC:L/AT:N/PR:N/UI:N/VC:N/VI:L/VA:H/SC:N/SI:L/SA:H
+        // Reported by @sepehrdaddev, VSS:4.0/AV:L/AC:L/AT:N/PR:N/UI:N/VC:N/VI:L/VA:H/SC:N/SI:L/SA:H
         // Crash due to depth limit on recursion.
         let code = include_str!("crash-recursion-depth.nasl");
         assert_eq!(code.len(), 587);
         let result = nasl_syntax::parse(code).collect::<Vec<_>>();
-        assert_eq!(result.len(), 1);
-        assert!(result[0].is_err())
+
+        assert_eq!(
+            result.iter().filter_map(|x| x.as_ref().ok()).count(),
+            0,
+            "crash-recursion-depth should not have any valid statements."
+        );
+
+        let code = include_str!("crash-prefix-recursion.nasl");
+        assert_eq!(code.len(), 515);
+        let result = nasl_syntax::parse(code).collect::<Vec<_>>();
+        assert_eq!(
+            result.iter().filter_map(|x| x.as_ref().ok()).count(),
+            0,
+            "crash-prefix-recursion should not have any valid statements."
+        );
     }
 }
