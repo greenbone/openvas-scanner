0b92bfe542616ea4fa770d7cfd86f7e087357153
Philipp Eder <philipp.eder@posteo.net>
2023-12-08 10:42:23 +0100

    Change: nasl-syntax: benchmark tests iterate
    Changes that the bench tests within nasl-syntax do indeed test all
    Statements instead of just the first.
    
    Additionally it tests smb_nt.inc as it is one of the largest nasl files
    within the community feed.

--- rust/nasl-syntax/benches/parse.rs
+++ rust/nasl-syntax/benches/parse.rs
@@ -2,31 +2,22 @@
 //
 // SPDX-License-Identifier: GPL-2.0-or-later
 
-use std::{env, fs, path::PathBuf};
-
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use nasl_syntax::parse;
 
-fn loadfile(filename: &str) -> PathBuf {
-    let mut current = env::current_dir().unwrap();
-    let reset = current.clone();
-    // move to nasl-syntax
-    current.push("nasl-syntax");
-    if !current.is_dir() {
-        // we were already in nasl-syntax and have to reset
-        current = reset;
-    }
-    current.push("benches");
-    current.push(filename);
-    current
-}
-
 pub fn simple_parse_benchmark(c: &mut Criterion) {
-    let code: String = fs::read(loadfile("simple_parse.nasl"))
-        .map(|bs| bs.iter().map(|&b| b as char).collect())
-        .unwrap();
-    c.bench_function("simple_parse", |b| b.iter(|| parse(black_box(&code))));
+    let code = include_str!("simple_parse.nasl");
+    c.bench_function("simple_parse", |b| {
+        b.iter(|| parse(black_box(&code)).map(|x| x.unwrap()).count())
+    });
 }
 
-criterion_group!(benches, simple_parse_benchmark);
+pub fn parse_large_benchmark(c: &mut Criterion) {
+    let code = include_str!("smb_nt.inc");
+    c.bench_function(&format!("smb_nt.inc {}", code.len()), |b| {
+        b.iter(|| parse(black_box(&code)).map(|x| x.unwrap()).count())
+    });
+}
+
+criterion_group!(benches, simple_parse_benchmark, parse_large_benchmark);
 criterion_main!(benches);
