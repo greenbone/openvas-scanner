0a768cd7b323d98a6baf5f4e3fbe85eb75105df0
Juan José Nicola <juan.nicola@greenbone.net>
2024-03-12 12:31:47 +0100

    Change: hide warning message in standalone mode (#1591)
    In standalone mode, there is no host process and IPC context are not initialized.
    Sending a message will always fail.

--- nasl/nasl_http.c
+++ nasl/nasl_http.c
@@ -99,7 +99,12 @@ _http_req (lex_ctxt *lexic, char *keyword)
       if (hostname == NULL)
         return NULL;
 
-      ua = g_strdup (user_agent_get (lexic->script_infos->ipc_context));
+      if ((user_agent_get (lexic->script_infos->ipc_context, &ua) == -2)
+          && !script_infos->standalone)
+        {
+          g_message ("Not possible to send the User Agent to the host process. "
+                     "Invalid IPC context");
+        }
       /* Servers should not have a problem with port 80 or 443 appended.
        * RFC2616 allows to omit the port in which case the default port for
        * that service is assumed.
