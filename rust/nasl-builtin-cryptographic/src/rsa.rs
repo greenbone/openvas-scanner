f9517e62466de1a6029fb3e4ef151f37f43858b1
= <henning.puetz@aol.com>
2024-05-17 17:18:07 +0200

    Add: NASL builtin function RSA

--- rust/nasl-builtin-cryptographic/src/rsa.rs
+++ rust/nasl-builtin-cryptographic/src/rsa.rs
@@ -4,7 +4,12 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 use nasl_builtin_utils::{Context, FunctionErrorKind, NaslFunction, Register};
-use rsa::{Pkcs1v15Encrypt, RsaPrivateKey, RsaPublicKey};
+use rsa::{
+    pkcs1::DecodeRsaPrivateKey,
+    pkcs1v15::{self, SigningKey},
+    sha2::{Digest, Sha256},
+    Oaep, Pkcs1v15Encrypt, Pkcs1v15Sign, RsaPrivateKey, RsaPublicKey,
+};
 use std::str;
 
 use crate::get_required_named_data;
@@ -49,10 +54,47 @@ fn rsa_private_decrypt<K>(
     return Ok(dec_data.to_vec().into());
 }
 
+fn rsa_sign<K>(
+    register: &Register,
+    _: &Context<K>,
+) -> Result<nasl_syntax::NaslValue, FunctionErrorKind> {
+    let data = get_required_named_data(register, "data")?;
+    let pem = get_required_named_data(register, "priv")?;
+    let passphrase = get_required_named_data(register, "passphrase")?;
+    let mut rng = rand::thread_rng();
+    let private_key = RsaPrivateKey::from_pkcs1_pem(std::str::from_utf8(pem).unwrap())
+        .expect("failed to generate a key");
+    let signing_key = SigningKey::<Sha256>::new(private_key);
+    let verifying_key = signing_key.verifying_key();
+    let signature = signing_key.sign_with_rng(&mut rng, data);
+    verifying_key
+        .verify(data, &signature)
+        .expect("failed to verify");
+    return Ok(verifying_key.to_vec().into());
+}
+
+fn rsa_public_decrypt<K>(
+    register: &Register,
+    _: &Context<K>,
+) -> Result<nasl_syntax::NaslValue, FunctionErrorKind> {
+    let sig = get_required_named_data(register, "sig")?;
+    let n = get_required_named_data(register, "n")?;
+    let e = get_required_named_data(register, "e")?;
+    let mut rng = rand::thread_rng();
+    let pub_key = RsaPublicKey::new(
+        rsa::BigUint::from_bytes_be(n),
+        rsa::BigUint::from_bytes_be(e),
+    )
+    .unwrap();
+    return Ok(sig.to_vec().into());
+}
+
 pub fn lookup<K>(key: &str) -> Option<NaslFunction<K>> {
     match key {
         "rsa_public_encrypt" => Some(rsa_public_encrypt),
         "rsa_private_decrypt" => Some(rsa_private_decrypt),
+        "rsa_sign" => Some(rsa_sign),
+        "rsa_public_decrypt" => Some(rsa_public_decrypt),
         _ => None,
     }
 }
