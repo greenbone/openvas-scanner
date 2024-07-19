e3b9e17396ed1693b81539c6980015f3683aeae7
Toni Peter <mail@tonipeter.de>
2024-07-19 13:56:00 +0200

    Allow &Context as argument to nasl_functions
    This is currently done by checking the argument type and seeing if it is
    a reference of something called "Context". Not the most precise
    implementation, but it would only fail when there is another type
    called Context.

--- rust/nasl-function-proc-macro/src/utils.rs
+++ rust/nasl-function-proc-macro/src/utils.rs
@@ -1,18 +1,21 @@
 use syn::*;
 
-pub fn subty_if_name<'a>(ty: &'a Type, name: &str) -> Option<&'a Type> {
-    subty_if(ty, |seg| seg.ident == name)
+pub fn ty_is_context(ty: &Type) -> bool {
+    if let Type::Reference(TypeReference { elem, .. }) = ty {
+        get_last_segment(elem).map(|seg| seg.ident == "Context").unwrap_or(false)
+    }
+    else {
+        false}
+    
 }
 
-pub fn subty_if<F>(ty: &Type, f: F) -> Option<&Type>
-where
-    F: FnOnce(&PathSegment) -> bool,
+pub fn get_subty_if_name_is<'a>(ty: &'a Type, name: &str) -> Option<&'a Type>
 {
-    only_last_segment(ty)
-        .filter(|segment| f(segment))
+    get_last_segment(ty)
+        .filter(|segment| segment.ident == name)
         .and_then(|segment| {
             if let PathArguments::AngleBracketed(args) = &segment.arguments {
-                only_one(args.args.iter()).and_then(|genneric| {
+                get_one(args.args.iter()).and_then(|genneric| {
                     if let GenericArgument::Type(ty) = genneric {
                         Some(ty)
                     } else {
@@ -25,7 +28,7 @@ where
         })
 }
 
-pub fn only_last_segment(mut ty: &Type) -> Option<&PathSegment> {
+fn get_last_segment(mut ty: &Type) -> Option<&PathSegment> {
     while let Type::Group(syn::TypeGroup { elem, .. }) = ty {
         ty = elem;
     }
@@ -37,13 +40,12 @@ pub fn only_last_segment(mut ty: &Type) -> Option<&PathSegment> {
                     leading_colon: None,
                     segments,
                 },
-        }) => only_one(segments.iter()),
-
+        }) => get_one(segments.iter()),
         _ => None,
     }
 }
 
-pub fn only_one<I, T>(mut iter: I) -> Option<T>
+fn get_one<I, T>(mut iter: I) -> Option<T>
 where
     I: Iterator<Item = T>,
 {
