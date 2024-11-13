b08ed4a02b5881ba50ef808573ddeff68fa68ae4
Toni Peter <mail@tonipeter.de>
2024-11-13 11:03:58 +0100

    Implement libssh connect more explicitly

--- rust/src/nasl/builtin/ssh/libssh/mod.rs
+++ rust/src/nasl/builtin/ssh/libssh/mod.rs
@@ -71,24 +71,6 @@ impl Ssh {
 
     /// Create a new session, but only add it to the list of active sessions
     /// if the given closure which modifies the session returns Ok(...).
-    pub async fn add_new_session(
-        &mut self,
-        f: impl Fn(&mut BorrowedSession) -> Result<()>,
-    ) -> Result<SessionId> {
-        let id = self.next_session_id()?;
-        let session = Mutex::new(SshSession::new(id)?);
-        {
-            let mut borrowed_session = session.lock().await;
-            if let Err(e) = f(&mut borrowed_session) {
-                borrowed_session.disconnect().await?;
-                return Err(e);
-            }
-        }
-        self.insert(id, session);
-        Ok(id)
-    }
-
-    #[allow(clippy::too_many_arguments)]
     pub async fn connect(
         &mut self,
         socket: Option<Socket>,
@@ -99,47 +81,81 @@ impl Ssh {
         scciphers: Vec<cipher::Name>,
         timeout: Option<Duration>,
     ) -> Result<SessionId> {
-        self.add_new_session(|session| {
-            let ip_str = ip.to_string();
-            session.set_option(SshOption::LogLevel(get_log_level()))?;
-            session.set_option(SshOption::Hostname(ip_str.clone()))?;
-            session.set_option(SshOption::KnownHosts(Some("/dev/null".to_owned())))?;
-            if let Some(timeout) = timeout {
-                session.set_option(SshOption::Timeout(timeout))?;
+        let id = self.next_session_id()?;
+        let session = Mutex::new(SshSession::new(id)?);
+        {
+            let mut borrowed_session = session.lock().await;
+            if let Err(e) = self
+                .try_connect(
+                    &mut borrowed_session,
+                    socket,
+                    ip,
+                    port,
+                    keytype,
+                    csciphers,
+                    scciphers,
+                    timeout,
+                )
+                .await
+            {
+                borrowed_session.disconnect().await?;
+                return Err(e);
             }
-            if let Some(keytype) = to_comma_separated_string(&keytype) {
-                session.set_option(SshOption::HostKeys(keytype))?;
-            }
-            if let Some(csciphers) = to_comma_separated_string(&csciphers) {
-                session.set_option(SshOption::CiphersCS(csciphers))?;
-            }
-            if let Some(scciphers) = to_comma_separated_string(&scciphers) {
-                session.set_option(SshOption::CiphersSC(scciphers))?;
-            }
-            session.set_option(SshOption::Port(port))?;
+        }
+        self.insert(id, session);
+        Ok(id)
+    }
 
-            if let Some(socket) = socket {
-                // This is a fake raw socket.
-                // TODO: implement openvas_get_socket_from_connection()
-                let my_sock = UdpSocket::bind("127.0.0.1:0").unwrap();
-                debug!(
-                    ip_str,
-                    sock_fd = my_sock.as_raw_fd(),
-                    nasl_sock = socket,
-                    "Setting SSH fd for socket",
-                );
-                session.set_option(SshOption::Socket(my_sock.as_raw_fd()))?;
-            }
+    #[allow(clippy::too_many_arguments)]
+    pub async fn try_connect(
+        &mut self,
+        session: &mut BorrowedSession<'_>,
+        socket: Option<Socket>,
+        ip: IpAddr,
+        port: u16,
+        keytype: Vec<key::Name>,
+        csciphers: Vec<cipher::Name>,
+        scciphers: Vec<cipher::Name>,
+        timeout: Option<Duration>,
+    ) -> Result<()> {
+        let ip_str = ip.to_string();
+        session.set_option(SshOption::LogLevel(get_log_level()))?;
+        session.set_option(SshOption::Hostname(ip_str.clone()))?;
+        session.set_option(SshOption::KnownHosts(Some("/dev/null".to_owned())))?;
+        if let Some(timeout) = timeout {
+            session.set_option(SshOption::Timeout(timeout))?;
+        }
+        if let Some(keytype) = to_comma_separated_string(&keytype) {
+            session.set_option(SshOption::HostKeys(keytype))?;
+        }
+        if let Some(csciphers) = to_comma_separated_string(&csciphers) {
+            session.set_option(SshOption::CiphersCS(csciphers))?;
+        }
+        if let Some(scciphers) = to_comma_separated_string(&scciphers) {
+            session.set_option(SshOption::CiphersSC(scciphers))?;
+        }
+        session.set_option(SshOption::Port(port))?;
+
+        if let Some(socket) = socket {
+            // This is a fake raw socket.
+            // TODO: implement openvas_get_socket_from_connection()
+            let my_sock = UdpSocket::bind("127.0.0.1:0").unwrap();
             debug!(
                 ip_str,
-                port = port,
-                socket = socket,
-                "Connecting to SSH server",
+                sock_fd = my_sock.as_raw_fd(),
+                nasl_sock = socket,
+                "Setting SSH fd for socket",
             );
-            session.connect()?;
-            Ok(())
-        })
-        .await
+            session.set_option(SshOption::Socket(my_sock.as_raw_fd()))?;
+        }
+        debug!(
+            ip_str,
+            port = port,
+            socket = socket,
+            "Connecting to SSH server",
+        );
+        session.connect()?;
+        Ok(())
     }
 
     pub async fn disconnect_and_remove(&mut self, session_id: SessionId) -> Result<()> {
