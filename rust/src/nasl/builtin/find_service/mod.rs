b90053dd8074e28015ac27382a480570cf05e367
Toni Peter <mail@tonipeter.de>
2024-12-10 11:47:15 +0100

    Implement basic setup for run_find_service

---
+++ rust/src/nasl/builtin/find_service/mod.rs
@@ -0,0 +1,41 @@
+// SPDX-FileCopyrightText: 2023 Greenbone AG
+//
+// SPDX-License-Identifier: GPL-2.0-or-later WITH x11vnc-openssl-exception
+
+use thiserror::Error;
+
+use crate::nasl::prelude::*;
+
+#[derive(Debug, Error)]
+pub enum FindServiceError {}
+
+struct Service {
+    name: String,
+    generate_result: GenerateResult,
+    save_banner: bool,
+    special_behavior: Option<SpecialBehavior>,
+}
+
+enum GenerateResult {
+    No,
+    Yes { is_vulnerability: bool },
+}
+
+enum SpecialBehavior {
+    // TODO fill this in for services
+}
+
+#[nasl_function]
+fn plugin_run_find_service() -> () {}
+
+#[derive(Default)]
+pub struct FindService {
+    services: Vec<Service>,
+}
+
+function_set! {
+    FindService,
+    (
+        plugin_run_find_service
+    )
+}
