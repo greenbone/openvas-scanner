95fc26b7873021fa97f81e2d09e4c7b60a5f242c
Toni Peter <mail@tonipeter.de>
2024-12-11 12:33:27 +0100

    Implement very basic port scanning

--- rust/src/nasl/builtin/find_service/mod.rs
+++ rust/src/nasl/builtin/find_service/mod.rs
@@ -2,12 +2,22 @@
 //
 // SPDX-License-Identifier: GPL-2.0-or-later WITH x11vnc-openssl-exception
 
+use std::{io, net::IpAddr, time::Duration};
+
 use thiserror::Error;
+use tokio::{io::AsyncReadExt, net::TcpStream, time::timeout};
 
 use crate::nasl::prelude::*;
 
+const TIMEOUT_MILLIS: u64 = 10000;
+
 #[derive(Debug, Error)]
-pub enum FindServiceError {}
+pub enum FindServiceError {
+    #[error("Failed to connect to TCP: {0}")]
+    TcpStreamConnect(io::Error),
+    #[error("Failed to read from TCP: {0}")]
+    TcpStreamRead(io::Error),
+}
 
 struct Service {
     name: String,
@@ -25,13 +35,53 @@ enum SpecialBehavior {
     // TODO fill this in for services
 }
 
-fn scan_port(port: u16) -> Option<Service> {
-    None
+async fn scan_port(target: IpAddr, port: u16) -> Result<Option<Service>, FindServiceError> {
+    let mut stream = TcpStream::connect((target, port))
+        .await
+        .map_err(|e| FindServiceError::TcpStreamConnect(e))?;
+
+    let mut buf: &mut [u8] = &mut [0; 10];
+    let result = timeout(Duration::from_millis(TIMEOUT_MILLIS), async move {
+        // let mut buffer: Vec<u8> = vec![];
+        let result = stream
+            .read(&mut buf)
+            .await
+            .map_err(|e| FindServiceError::TcpStreamRead(e));
+        dbg!(result?);
+        Ok::<_, FindServiceError>(buf)
+    })
+    .await;
+    match result {
+        Ok(Ok(buf)) => {
+            println!("{}", String::from_utf8(buf.to_vec()).unwrap());
+        }
+        Ok(Err(_)) => {
+            println!("err");
+        }
+        Err(_) => {
+            println!("{}", port);
+            println!("timeout");
+        }
+    }
+    Ok(None)
+
+    // # open socket
+    //             socket.connect(( str(sys.argv[1] + '.' + str(host)), int(port) ))
+    //             print('Connecting to' + str(sys.argv[1]) + 'on the port' + str(port))
+    //             # segs until timeout
+    //             socket.settimeout(1)
+    //             # getting the banner from the server
+    //             banner = socket.recv(1024)
 }
 
 #[nasl_function]
-fn plugin_run_find_service(context: &Context) -> () {
-    for port in context.port_range() {}
+async fn plugin_run_find_service(context: &Context<'_>) -> () {
+    for port in context.port_range() {
+        match scan_port(context.target_ip(), port).await {
+            Ok(_) => {}
+            Err(e) => {}
+        }
+    }
 }
 
 #[derive(Default)]
