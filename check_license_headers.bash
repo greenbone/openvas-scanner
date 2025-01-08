d9e16f4f275f5cec15aa9d93d46b05ab84b12ee8
Toni Peter <mail@tonipeter.de>
2025-01-08 15:21:19 +0100

    Fix minor things in the script:
    1. Fix whitespace splitting due to forloop
    2. Adjust comment string depending on file extension
    3. Use all directories except for specific ones
    4. Add nasl and cmake files.

--- check_license_headers.bash
+++ check_license_headers.bash
@@ -1,10 +1,20 @@
 #!/usr/bin/env bash
 
+function comment_string () {
+    ext=$1
+    if [[ $ext == "c" || $ext == "h" || $ext == "rs" ]]; then
+        echo "//"
+    elif [[ $ext == "nasl" || $ext == "cmake" ]]; then
+        echo "#"
+    fi
+}
+
 any_missing_headers=0
 
-for folder in src misc rust/src; do
-    echo "Checking $folder"
-    for f in $(find $folder -regex ".*\.\(rs\|c\|h\)"); do
+exts="c h nasl cmake"
+
+for ext in $exts; do
+    find . -not -path "./rust/target/*" -not -path "./rust/crates/nasl-c-lib/tmp/*" -regex ".*\.\($ext\)" -print0 | while read -d $'\0' f; do
         header=$(head -n 3 "$f")
         if ! [[ "$header" =~ SPDX ]]; then
             echo "File does not contain license header: $f"
@@ -13,7 +23,8 @@ for folder in src misc rust/src; do
             if [[ "$1" == add_header ]]; then
                 tmpfile=$(mktemp)
                 cp "$f" "$tmpfile"
-                echo -e "// SPDX-FileCopyrightText: 2025 Greenbone AG\n//\n// SPDX-License-Identifier: GPL-2.0-or-later WITH x11vnc-openssl-exception\n" | cat - $tmpfile > "$f"
+                comment=$(comment_string $ext)
+                echo -e "$comment SPDX-FileCopyrightText: 2025 Greenbone AG\n$comment\n$comment SPDX-License-Identifier: GPL-2.0-or-later WITH x11vnc-openssl-exception\n" | cat - $tmpfile > "$f"
             fi
         fi
     done
