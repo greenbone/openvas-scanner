88ebfccb030c8c542bd14659d5c79b62a1295e7f
Toni Peter <mail@tonipeter.de>
2025-03-13 15:43:03 +0100

    Move Statement to grammar mod

--- rust/src/nasl/syntax/mod.rs
+++ rust/src/nasl/syntax/mod.rs
@@ -12,17 +12,16 @@ mod loader;
 mod naslvalue;
 mod operation;
 mod prefix_extension;
-mod statement;
 mod token;
 mod tokenizer;
 
 pub use crate::storage::item::ACT;
 pub use error::{ErrorKind, SyntaxError};
 pub use grammar::Ast;
+pub use grammar::{AssignOrder, Statement, StatementKind};
 pub(super) use lexer::Lexer;
 pub use loader::*;
 pub use naslvalue::*;
-pub use statement::*;
 pub use token::Ident;
 pub use token::Keyword;
 pub use token::Token;
