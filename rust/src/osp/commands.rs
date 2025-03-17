0f27d0bdc70c2c1530bcef5317f6b31acf3bbc67
Juan Jose Nicola <juan.nicola@greenbone.net>
2025-03-17 13:25:25 -0300

    Fix: Revert #1805

--- rust/src/osp/commands.rs
+++ rust/src/osp/commands.rs
@@ -188,12 +188,7 @@ fn write_scanner_prefs(scan: &Scan, writer: &mut Writer) -> Result<()> {
     writer.write_event(Event::Start(BytesStart::new("scanner_params")))?;
     for p in &scan.scan_preferences {
         writer.write_event(Event::Start(BytesStart::new(&p.id)))?;
-        let value = match p.value.as_ref() {
-            "yes" => "1",
-            "no" => "0",
-            v => v,
-        };
-        writer.write_event(Event::Text(BytesText::new(value)))?;
+        writer.write_event(Event::Text(BytesText::new(&p.value)))?;
         writer.write_event(Event::End(BytesEnd::new(&p.id)))?;
     }
 
