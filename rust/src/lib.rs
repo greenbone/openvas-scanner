c1b6971603d5074fd139b21684eae0a30fd1e846
Toni Peter <mail@tonipeter.de>
2025-05-20 13:13:59 +0200

    Allow clippy::result_large_err for now

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -2,6 +2,11 @@
 //
 // SPDX-License-Identifier: GPL-2.0-or-later WITH x11vnc-openssl-exception
 
+// We allow this fow now, since it would require lots of changes
+// but should eventually solve this.
+#![allow(clippy::result_large_err)]
+#![allow(clippy::large_enum_variant)]
+
 #[cfg(feature = "nasl-builtin-raw-ip")]
 pub mod alive_test;
 pub mod feed;
