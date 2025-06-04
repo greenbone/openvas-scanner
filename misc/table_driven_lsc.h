4662f8be45d23801d03d9ec8c55c3f90977f0771
Kraemii <c.kraemer.rulle@web.de>
2025-06-04 11:06:50 +0200

    Show Notus results in a JSON like defined Structure
    Structure:
    ```json
    [
    	0: [return code],
    	1: {
    		"oid": "[oid]",
    		"vulnerable_packages": [{
    			"name": "[package_name]",
    			"installed": "[installed_version]",
    			"fixed": {
    				"version": "[fixed_version]"
    				"specifier": "[specifier]"
    			}
    		}]
    	},
    	2: {
    		"oid": "[oid]",
    		"vulnerable_packages": [{
    			"name": "[package_name]",
    			"installed": "[installed_version]",
    			"fixed": {
    				"start": "[start_version]",
    				"end": "[end_version]"
    			}
    		}]
    	}
    	.
    	.
    	.
    ]
    ```

--- misc/table_driven_lsc.h
+++ misc/table_driven_lsc.h
@@ -14,6 +14,74 @@
 #include <glib.h>
 #include <gvm/util/kb.h> // for kb_t
 
+/** @brief Fixed version format
+ */
+enum fixed_type
+{
+  UNKNOWN, // Unknown
+  RANGE,   // Range of version which fixed the package
+  SINGLE,  // A single version with a specifier (gt or lt)
+};
+
+/** @brief Fixed version
+ */
+struct fixed_version
+{
+  char *version;   // a version
+  char *specifier; // a lt or gt specifier
+};
+typedef struct fixed_version fixed_version_t;
+
+/** @brief Specify a version range
+ */
+struct version_range
+{
+  char *start; // <= the version
+  char *stop;  // >= the version
+};
+typedef struct version_range version_range_t;
+
+/** @brief Define a vulnerable package
+ */
+struct vulnerable_pkg
+{
+  char *pkg_name;        // package name
+  char *install_version; // installed version of the vulnerable package
+  enum fixed_type type;  // fixed version type: range or single
+  union
+  {
+    version_range_t *range;   // range of vulnerable versions
+    fixed_version_t *version; // version and specifier for the fixed versions
+  };
+};
+
+typedef struct vulnerable_pkg vuln_pkg_t;
+
+/** brief define an advisory with a list of vulnerable packages
+ */
+struct advisory
+{
+  char *oid;             // Advisory OID
+  vuln_pkg_t *pkgs[100]; // list of vulnerable packages, installed version and
+                         // fixed versions
+  size_t count;          // Count of vulnerable packages this advisory has
+};
+
+typedef struct advisory advisory_t;
+
+/** brief define a advisories list
+ */
+struct advisories
+{
+  advisory_t **advisories;
+  size_t count;
+  size_t max_size;
+};
+typedef struct advisories advisories_t;
+
+void
+advisories_free (advisories_t *advisories);
+
 void
 set_lsc_flag (void);
 
@@ -26,5 +94,8 @@ run_table_driven_lsc (const char *, const char *, const char *, const char *,
 
 char *
 notus_get_response (const char *pkg_list, const char *os);
+
+advisories_t *
+process_notus_response (const gchar *resp, const size_t len);
 
 #endif // MISC_TABLE_DRIVEN_LSC_H
