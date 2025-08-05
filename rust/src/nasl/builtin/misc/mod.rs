d7bf207afcc8ceaa643f69aad675c5997ac3e8ba
Juan Jose Nicola <juan.nicola@greenbone.net>
2025-08-05 13:55:12 -0300

    Add: safe_checks and get_script_oid() nasl functions

--- rust/src/nasl/builtin/misc/mod.rs
+++ rust/src/nasl/builtin/misc/mod.rs
@@ -290,6 +290,26 @@ fn vendor_version(context: &ScanCtx<'_>) -> Result<NaslValue, MiscError> {
     }
 }
 
+/// Are safe checks enabled ?
+#[nasl_function]
+fn safe_checks(context: &ScanCtx<'_>) -> NaslValue {
+    NaslValue::Boolean(
+        context
+            .scan_preferences
+            .get_preference_bool("safe_checks")
+            .unwrap_or(false),
+    )
+}
+
+/// Are safe checks enabled ?
+#[nasl_function]
+fn get_script_oid(context: &ScanCtx<'_>) -> NaslValue {
+    match context.nvt().clone() {
+        Some(nvt) => NaslValue::Data(nvt.oid.into_bytes()),
+        None => NaslValue::Null,
+    }
+}
+
 pub struct Misc;
 
 function_set! {
@@ -311,6 +331,8 @@ function_set! {
         gettimeofday,
         dump_ctxt,
         vendor_version,
+        safe_checks,
+        get_script_oid,
     )
 }
 
