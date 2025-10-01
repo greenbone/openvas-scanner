b2592e8767db9d59e6db069062b4a5ef418f2af3
Toni Peter <mail@tonipeter.de>
2025-10-01 11:15:10 +0200

    Address review comments

--- rust/openvasd-tests/entrypoint.sh
+++ rust/openvasd-tests/entrypoint.sh
@@ -1,17 +1,22 @@
 #!/usr/bin/env bash
 set -e
 
-OPENVASD_PATH=/opt/openvasd_target/debug/openvasd
+PROFILE=${PROFILE:-release}
+OPENVASD_PATH=/opt/openvasd_target/${PROFILE}/openvasd
 
-redis-server --daemonize yes --bind 127.0.0.1 --port 6379
+mkdir -p /run/redis
+redis-server /etc/redis/redis.conf
 
-until redis-cli ping > /dev/null 2>&1; do
+until redis-cli -s /run/redis/redis.sock ping > /dev/null 2>&1; do
     echo "Waiting for Redis..."
     sleep 1
 done
 
 export API_KEY=${API_KEY:-changeme}
 
+# A small hack so simply copying the binary works even if
+# the host system is NixOS. This doesn't have any effect
+# for other systems.
 if [ -f  $OPENVASD_PATH ]; then
     patchelf --set-interpreter /lib64/ld-linux-x86-64.so.2 $OPENVASD_PATH 2>/dev/null || true
     chmod +x $OPENVASD_PATH
@@ -26,9 +31,6 @@ type = "${SCANNER_TYPE:-openvas}"
 
 [storage]
 type = "redis"
-
-[storage.redis]
-url = "127.0.0.1:6379"
 
 [listener]
 address = "0.0.0.0:3000"
