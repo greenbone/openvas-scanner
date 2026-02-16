9a94a7510d463086962eaa0fd3251a624d1268c6
Kraemii <c.kraemer.rulle@web.de>
2026-02-16 18:28:59 +0100

    Add: Nasl builtin functions for Kerberos support
    This includes:
    - krb5_error_code_to_string
    - krb5_find_kdc
    - krb5_gss_init
    - krb5_gss_prepare_context
    - krb5_gss_session_key
    - krb5_gss_update_context
    - krb5_gss_update_context_needs_more
    - krb5_gss_update_context_out
    - krb5_is_failure
    - krb5_is_success

--- rust/src/nasl/builtin/error.rs
+++ rust/src/nasl/builtin/error.rs
@@ -4,6 +4,8 @@
 
 use thiserror::Error;
 
+#[cfg(feature = "nasl-c-lib")]
+use crate::nasl::builtin::krb5::Krb5Error;
 use crate::nasl::prelude::*;
 use crate::nasl::utils::error::FnErrorKind;
 
@@ -54,6 +56,9 @@ pub enum BuiltinError {
     RawIp(RawIpError),
     #[error("{0}")]
     Preference(String),
+    #[cfg(feature = "nasl-c-lib")]
+    #[error("{0}")]
+    Krb5(Krb5Error),
 }
 
 macro_rules! builtin_error_variant (
@@ -100,3 +105,6 @@ builtin_error_variant!(SysError, Sys);
 builtin_error_variant!(FindServiceError, FindService);
 builtin_error_variant!(SnmpError, Snmp);
 builtin_error_variant!(RawIpError, RawIp);
+
+#[cfg(feature = "nasl-c-lib")]
+builtin_error_variant!(Krb5Error, Krb5);
