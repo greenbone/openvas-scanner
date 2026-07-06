23096dbf8bfffb9ffad5e7cce2eab0ed31f87e23
Toni Peter <mail@tonipeter.de>
2026-07-06 10:53:46 +0200

    Replace libgcrypt aeg-gmac with rust-only impl

--- rust/src/nasl/builtin/cryptographic/aes_gmac.rs
+++ rust/src/nasl/builtin/cryptographic/aes_gmac.rs
@@ -4,16 +4,77 @@
 
 use crate::function_set;
 use crate::nasl::{prelude::*, utils::function::StringOrData};
+use aes::{
+    Aes128, Aes192, Aes256,
+    cipher::{BlockCipher, BlockEncrypt, BlockSizeUser},
+};
+use aes_gcm::{
+    AesGcm, Nonce,
+    aead::{AeadInPlace, generic_array::ArrayLength},
+};
+use digest::typenum::{U1, U2, U3, U4, U5, U6, U7, U8, U9, U10, U11, U12, U13, U14, U15, U16};
+
+use super::CryptographicError;
 
 pub fn aes_gmac(data: &[u8], key: &[u8], iv: &[u8]) -> Result<NaslValue, FnError> {
-    use nasl_c_lib::crypt::mac::aes_gmac;
-
-    use crate::nasl::builtin::cryptographic::CryptographicError;
-
-    match aes_gmac(data, key, iv) {
-        Ok(val) => Ok(val.into()),
-        Err(msg) => Err(CryptographicError::AesGmacError(msg.to_string()).into()),
+    if key.is_empty() {
+        return Err(CryptographicError::AesGmacError("Missing key".to_string()).into());
     }
+    if data.is_empty() {
+        return Err(CryptographicError::AesGmacError("Missing value".to_string()).into());
+    }
+    if iv.is_empty() {
+        return Err(CryptographicError::AesGmacError("General error".to_string()).into());
+    }
+
+    match key.len() {
+        16 => aes_gmac_with_key::<Aes128>(data, key, iv),
+        24 => aes_gmac_with_key::<Aes192>(data, key, iv),
+        32 => aes_gmac_with_key::<Aes256>(data, key, iv),
+        _ => Err(CryptographicError::AesGmacError("Invalid key length".to_string()).into()),
+    }
+}
+
+fn aes_gmac_with_key<D>(data: &[u8], key: &[u8], iv: &[u8]) -> Result<NaslValue, FnError>
+where
+    D: BlockCipher + BlockSizeUser<BlockSize = U16> + BlockEncrypt + aes::cipher::KeyInit,
+{
+    match iv.len() {
+        1 => aes_gmac_with_nonce::<D, U1>(data, key, iv),
+        2 => aes_gmac_with_nonce::<D, U2>(data, key, iv),
+        3 => aes_gmac_with_nonce::<D, U3>(data, key, iv),
+        4 => aes_gmac_with_nonce::<D, U4>(data, key, iv),
+        5 => aes_gmac_with_nonce::<D, U5>(data, key, iv),
+        6 => aes_gmac_with_nonce::<D, U6>(data, key, iv),
+        7 => aes_gmac_with_nonce::<D, U7>(data, key, iv),
+        8 => aes_gmac_with_nonce::<D, U8>(data, key, iv),
+        9 => aes_gmac_with_nonce::<D, U9>(data, key, iv),
+        10 => aes_gmac_with_nonce::<D, U10>(data, key, iv),
+        11 => aes_gmac_with_nonce::<D, U11>(data, key, iv),
+        12 => aes_gmac_with_nonce::<D, U12>(data, key, iv),
+        13 => aes_gmac_with_nonce::<D, U13>(data, key, iv),
+        14 => aes_gmac_with_nonce::<D, U14>(data, key, iv),
+        15 => aes_gmac_with_nonce::<D, U15>(data, key, iv),
+        16 => aes_gmac_with_nonce::<D, U16>(data, key, iv),
+        _ => Err(CryptographicError::AesGmacError("Invalid IV length".to_string()).into()),
+    }
+}
+
+fn aes_gmac_with_nonce<D, N>(data: &[u8], key: &[u8], iv: &[u8]) -> Result<NaslValue, FnError>
+where
+    D: BlockCipher + BlockSizeUser<BlockSize = U16> + BlockEncrypt + aes::cipher::KeyInit,
+    N: ArrayLength<u8>,
+{
+    use aes_gcm::KeyInit;
+
+    let cipher = AesGcm::<D, N>::new_from_slice(key)
+        .map_err(|_| CryptographicError::AesGmacError("Invalid key length".to_string()))?;
+    let mut empty_plaintext = [];
+    let tag = cipher
+        .encrypt_in_place_detached(Nonce::<N>::from_slice(iv), data, &mut empty_plaintext)
+        .map_err(|_| CryptographicError::AesGmacError("Unable to calculate GMAC".to_string()))?;
+
+    Ok(tag.to_vec().into())
 }
 
 /// NASL function to calculate GMAC with AES128.
