cc3ea5ae2ced3d6b9e3f57923111ce19a9cbfff0
Toni Peter <mail@tonipeter.de>
2026-07-30 10:26:47 +0200

    Remove single-impl `NotusStorage` trait.

--- rust/src/scannerctl/notus_update/update.rs
+++ rust/src/scannerctl/notus_update/update.rs
@@ -16,31 +16,19 @@ use scannerlib::notus::advisories::VulnerabilityData;
 use scannerlib::notus::advisory_loader;
 use scannerlib::storage::Dispatcher;
 use scannerlib::storage::items::notus_advisory::NotusCache;
-use scannerlib::storage::redis::RedisAddAdvisory;
-use scannerlib::storage::redis::RedisAddNvt;
-use scannerlib::storage::redis::RedisGetNvt;
+use scannerlib::storage::redis::RedisCtx;
 use scannerlib::storage::redis::RedisStorage;
-use scannerlib::storage::redis::RedisWrapper;
-
-pub(crate) trait NotusStorage:
-    Dispatcher<(), Item = VulnerabilityData> + Dispatcher<NotusCache, Item = ()>
-{
-}
-
-impl<S> NotusStorage for RedisStorage<S> where
-    S: RedisWrapper + RedisAddNvt + RedisAddAdvisory + RedisGetNvt + Send
-{
-}
 
 pub fn signature_error(e: impl std::fmt::Display) -> CliError {
     CliErrorKind::LoadError(LoadError::Dirty(e.to_string()))
         .with(Filename(Path::new(feed::Hasher::Sha256.sum_file())))
 }
 
-pub async fn run<S>(storage: S, path: PathBuf, signature_check: bool) -> Result<(), CliError>
-where
-    S: NotusStorage,
-{
+pub async fn run(
+    storage: RedisStorage<RedisCtx>,
+    path: PathBuf,
+    signature_check: bool,
+) -> Result<(), CliError> {
     let loader = Loader::from_feed_path(path);
     let advisories_files = match advisory_loader(signature_check, &loader) {
         Ok(loader) => loader,
